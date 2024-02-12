ff42b68471b654549119bfee90ec707ba72a5759
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-02-11 23:15:24 -0500

    block max gas

--- scripts/init_consumer_assign_keys.sh
+++ scripts/init_consumer_assign_keys.sh
@@ -38,7 +38,7 @@ mv genesis-1.json $CONSUMER_HOME_1/config/genesis.json
 
 echo "Patching genesis for block max gas != -1..."
 jq -r '.consensus_params.block.max_gas = "50000000"' $CONSUMER_HOME_1/config/genesis.json > consumer-gas.json
-MV consumer-gas.json $CONSUMER_HOME_1/config/genesis.json
+mv consumer-gas.json $CONSUMER_HOME_1/config/genesis.json
 
 # Set slashing to $DOWNTIME_BLOCKS
 jq -r --arg SLASH "$DOWNTIME_BLOCKS" '.app_state.slashing.params.signed_blocks_window |= $SLASH' $CONSUMER_HOME_1/config/genesis.json > consumer-slashing.json
