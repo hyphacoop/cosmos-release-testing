203d3fef4d38f8c90bb6db0e78ec0e16d3d7ca69
ASoTNetworks <dev@aseriesoftubez.com>
2024-02-27 18:54:11 -0500

    update hermes config location

--- scripts/stateful/setup_relayer.sh
+++ scripts/stateful/setup_relayer.sh
@@ -35,7 +35,7 @@ echo "Set hermes parth"
 export PATH="$PATH:~/.hermes"
 
 echo "Setting up Hermes config..."
-cp tests/major_stateful_upgrade/hermes-config.toml ~/.hermes/config.toml
+cp templates/hermes-config-stateful.toml ~/.hermes/config.toml
 
 echo "Adding relayer keys..."
 echo $MNEMONIC_4 > mnemonic.txt
