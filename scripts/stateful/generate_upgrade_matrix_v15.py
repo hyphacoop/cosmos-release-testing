fcbaecbc3aeed85c466121315d355a0d17d58539
ASoTNetworks <dev@aseriesoftubez.com>
2024-03-11 13:37:10 -0400

    do not run on push and update starting version

--- scripts/stateful/generate_upgrade_matrix_v15.py
+++ scripts/stateful/generate_upgrade_matrix_v15.py
@@ -4,7 +4,7 @@ import json
 import sys
 import re
 
-SKIP_STARTING_VERSIONS = ['v14.0.0','v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2','v15.0.0-rc3','v15.0.0']
+SKIP_STARTING_VERSIONS = ['v14.0.0','v14.1.0','v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2','v15.0.0-rc3','v15.0.0']
 SKIP_TARGET_VERSIONS = ['v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2']
 
 # Must provide a cutoff version, e.g. 'v6.0.4'
