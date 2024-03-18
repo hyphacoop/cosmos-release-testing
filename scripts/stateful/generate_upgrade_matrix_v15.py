96ab345f5fde8ee0097732df471a8f93fa3506f9
ASoTNetworks <dev@aseriesoftubez.com>
2024-03-18 09:29:37 -0400

    test v15.1.0-rc1

--- scripts/stateful/generate_upgrade_matrix_v15.py
+++ scripts/stateful/generate_upgrade_matrix_v15.py
@@ -5,7 +5,7 @@ import sys
 import re
 
 SKIP_STARTING_VERSIONS = ['v14.0.0','v14.1.0','v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2','v15.0.0-rc3','v15.0.0']
-SKIP_TARGET_VERSIONS = ['v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2','v15.0.0-rc3']
+SKIP_TARGET_VERSIONS = ['v15.0.0-rc0','v15.0.0-rc1','v15.0.0-rc2','v15.0.0-rc3','v15.0.0','v15.1.0-rc0']
 
 # Must provide a cutoff version, e.g. 'v6.0.4'
 starting_version = sys.argv[1].split('.')
