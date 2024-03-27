8b6c14ffef3fe9e3e91aca19b83f165742c0ffe6
violet <violet@hypha.coop>
2024-03-27 14:13:40 -0400

    Tests for v16 ibc rate limiting

---
+++ interchaintests/fresh/v16_ibc_rate_limiting_test.go
@@ -0,0 +1,21 @@
+package fresh_test
+
+import (
+	"testing"
+
+	"github.com/hyphacoop/cosmos-release-testing/interchaintests/fresh"
+	"github.com/stretchr/testify/require"
+)
+
+func TestV16UpgradeIBCRateLimiting(t *testing.T) {
+	ctx, err := fresh.NewTestContext(t)
+	require.NoError(t, err)
+
+	chainA, chainB, relayer := fresh.CreateLinkedChains(ctx, t, fresh.GetConfig(ctx).StartVersion)
+	channel, err := fresh.GetTransferChannel(ctx, chainA, relayer)
+	require.NoError(t, err)
+
+	fresh.UpgradeChain(ctx, t, chainA, fresh.VALIDATOR_MONIKER, fresh.GetConfig(ctx).TargetVersion, fresh.GetConfig(ctx).UpgradeVersion)
+
+	fresh.IBCTransferRateLimitedTest(ctx, t, chainA, chainB, channel)
+}
