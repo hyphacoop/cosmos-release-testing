01ed2d6483cde66c6af1d312f606d54c532c15d8
ASoTNetworks <dev@aseriesoftubez.com>
2024-03-28 23:11:40 -0400

    wait for block after tx

--- tests/stateful/test_tx.sh
+++ tests/stateful/test_tx.sh
@@ -9,7 +9,7 @@ check_code()
   try=1
   txhash=$1
   while [ $try -lt 5 ]; do
-    code=$($CHAIN_BINARY q tx --type=hash $txhash -o json --home $HOME_1 | jq '.code')
+    code=$($CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.code')
     echo "Code is: $code"
     if [ -z $code ]; then
       echo "code returned blank, tx was unsuccessful. Try: $try"
@@ -33,12 +33,14 @@ check_code()
 
 echo "Sending funds with tx bank send..."
 TXHASH=$($CHAIN_BINARY tx bank send $WALLET_1 $WALLET_2 $VAL_STAKE_STEP$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b sync | jq '.txhash' | tr -d '"')
-sleep 20
+# wait for 1 block
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 check_code $TXHASH
 
 echo "Delegating funds from test account to validator..."
 TXHASH=$($CHAIN_BINARY tx staking delegate $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_2 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b sync | jq '.txhash' | tr -d '"')
-sleep 20
+# wait for 1 block
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 check_code $TXHASH
 
 # Wait for rewards to accumulate
@@ -48,6 +50,8 @@ echo "Withdrawing rewards for test account..."
 starting_balance=$($CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
 echo "Starting balance: $starting_balance"
 TXHASH=$($CHAIN_BINARY tx distribution withdraw-rewards $VALOPER_1 --home $HOME_1 --from $MONIKER_2 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b sync | jq '.txhash' | tr -d '"')
+# wait for 1 block
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 check_code $TXHASH
 sleep 10
 
@@ -66,4 +70,6 @@ fi
 
 echo "Unbonding funds from test account to validator..."
 TXHASH=$($CHAIN_BINARY tx staking unbond $VALOPER_1 $VAL_STAKE$DENOM --home $HOME_1 --from $MONIKER_1 --keyring-backend test --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $HIGH_FEES$DENOM --chain-id $CHAIN_ID -y -o json -b sync | jq '.txhash' | tr -d '"')
+# wait for 1 block
+tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 check_code $TXHASH
