29997360d922ac83479ce9c5d03ab6fca3684580
Dante <dante.a.sanchez@gmail.com>
2024-04-14 21:46:57 -0400

    channel limits

--- scripts/v16/setup_channels_rate_limit.sh
+++ scripts/v16/setup_channels_rate_limit.sh
@@ -10,7 +10,8 @@ hermes create client --host-chain $CHAIN_1 --reference-chain $CHAIN_2
 hermes create client --reference-chain $CHAIN_1 --host-chain $CHAIN_2
 hermes create connection --a-chain $CHAIN_1 --a-client 07-tendermint-$client_count --b-client 07-tendermint-0
 
-for i in {$start_channel..$end_channel};
+# for i in {$start_channel..$end_channel};
+for ((i=$start_channel; i<=$end_channel; i++))
 do
     echo "creating channel-$i..."
     hermes create channel --a-chain $CHAIN_1 --a-connection connection-$client_count --a-port transfer --b-port transfer
