55045fc877b2ad3d65d8bc769d7fefe57d61cf47
ASoTNetworks <dev@aseriesoftubez.com>
2024-04-19 22:18:53 -0400

    fix mem-csv script

--- scripts/mem-csv.sh
+++ scripts/mem-csv.sh
@@ -6,10 +6,9 @@ while true
 do
     pid=$(pidof $1)
     if [ ! -z "$pid" ] ; then
-        ps -o rss= $(pidof $1) | \
         ps -o rss= $(pidof htop) | awk '{printf strftime("%s")",%.0f\n", $1 / 1024 * 1024 *1024}'
     else
         echo "$(date +"%s"),0"
-    fi > $2
+    fi >> $2
     sleep 1
 done
