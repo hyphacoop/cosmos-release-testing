16378344b83c739cfdac6ef90ca82e5f9ec73512
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-04-21 21:47:44 -0400

    test ibc fee middleware

---
+++ tests/gaia-v16/test_ibc-fee.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+balance_before=$($CHAIN_BINARY q bank balances $RELAYER_WALLET --home $HOME_1 -o json | jq -r --arg DENOM "$DENOM" '.balances[] | select(.denom == $DENOM).amount')
+
+$RELAYER --json create channel --order unordered --a-chain $CHAIN_ID --a-port transfer --b-port transfer --a-connection connection-0 --channel-version "{\"fee_version\":\"ics29-1\",\"app_version\":\"ics20-1\"}"
+
+txhash=$($CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 $WALLET_1 1$DENOM --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT --from $WALLET_1 --output json -y --home $HOME_1 | jq -r '.txhash')
+sleep $TIMEOUT_COMMIT
+sequence=$($CHAIN_BINARY q tx $txhash --home $HOME_1 -o json | jq -r '.events[] | select(.type == "send_packet").attributes[] | select(.key == "packet_sequence").value')
+$CHAIN_BINARY tx ibc-fee pay-packet-fee transfer channel-0 $sequence --recv-fee 123uatom --ack-fee 456uatom --timeout-fee 789uatom --from $WALLET_1 --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT --home $HOME_1 -y
+
+sleep 30
+balance_after=$($CHAIN_BINARY q bank balances $RELAYER_WALLET --home $HOME_1 -o json | jq -r --arg DENOM "$DENOM" '.balances[] | select(.denom == $DENOM).amount')
+
+difference=$(echo "$balance_after - $balance_before" | bc)
+echo "Difference: ${difference}$DENOM"
