d9996e9e8d196dcc1c75607343d1dc98d0c4d4ce
ASoTNetworks <dev@aseriesoftubez.com>
2024-05-06 15:47:43 -0400

    debug print q ratelimit rate-limit output

--- tests/gaia-v16/stateful/test_rate_limits.sh
+++ tests/gaia-v16/stateful/test_rate_limits.sh
@@ -38,6 +38,7 @@ fi
 
 echo "Bank total supply: $($CHAIN_BINARY q bank total --home $HOME_1 -o json | jq -r '.supply[0].amount')"
 new_supply=$($CHAIN_BINARY q ratelimit rate-limit $channel_id --home $HOME_1 -o json | jq -r '.[0].flow.channel_value')
+$CHAIN_BINARY q ratelimit rate-limit $channel_id --home $HOME_1
 echo "New rate limit channel value: $new_supply"
 fraction=$(echo "$rate_limit / 2" | bc -l )
 amount=$(echo "($supply *  $fraction)/1 + 1000000" | bc )
