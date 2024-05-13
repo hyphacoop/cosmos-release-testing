f3ab508942b8ddba91f5a3a57a075eb60a69ded1
violet <violet@hypha.coop>
2024-05-13 12:55:46 -0400

    Test with fork binary

---
+++ scripts/stateful/fork_chain.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# 1. Set up a two-validator provider chain.
+set -ex
+source ~/env/bin/activate
+
+# Download archived home directory
+echo "Initializing node homes..."
+echo "Downloading and extracting archived state"
+mkdir -p $HOME_1 
+wget -qO- $HOME/archived-state.gz $ARCHIVE_URL | tar vxz -C $HOME_1 --strip-components=1
+
+echo "Patching config files..."
+# app.toml
+# minimum_gas_prices
+sed -i -e "/minimum-gas-prices =/ s^= .*^= \"0.0025$DENOM\"^" $HOME_1/config/app.toml
+# Enable API
+toml set --toml-path $HOME_1/config/app.toml api.enable true
+# Set different ports for api
+toml set --toml-path $HOME_1/config/app.toml api.address "tcp://0.0.0.0:$VAL1_API_PORT"
+# Set different ports for grpc
+toml set --toml-path $HOME_1/config/app.toml grpc.address "0.0.0.0:$VAL1_GRPC_PORT"
+# Turn off grpc web
+toml set --toml-path $HOME_1/config/app.toml grpc-web.enable false
+# config.toml
+# Replace fast_sync with block_sync
+sed -i -e "s\fast_sync\block_sync\g" $HOME_1/config/config.toml
+# Set different ports for rpc
+toml set --toml-path $HOME_1/config/config.toml rpc.laddr "tcp://0.0.0.0:$VAL1_RPC_PORT"
+# Set different ports for rpc pprof
+toml set --toml-path $HOME_1/config/config.toml rpc.pprof_laddr "localhost:$VAL1_PPROF_PORT"
+# Set different ports for p2p
+toml set --toml-path $HOME_1/config/config.toml p2p.laddr "tcp://0.0.0.0:$VAL1_P2P_PORT"
+# Allow duplicate IPs in p2p
+toml set --toml-path $HOME_1/config/config.toml p2p.allow_duplicate_ip true
+# Set client ports for rpc
+toml set --toml-path $HOME_1/config/client.toml node "tcp://localhost:$VAL1_RPC_PORT"
+# Set client chain-id
+toml set --toml-path $HOME_1/config/client.toml chain-id "$CHAIN_ID"
+
+GAIA_BRANCH="release/${START_VERSION%%.*}.x"
+git clone https://github.com/cosmos/gaia.git
+cd gaia
+git checkout $GAIA_BRANCH
+make build BUILD_TAGS="-tag unsafe_start_local_validator"
+cp build/$CHAIN_BINARY $HOME/go/bin/$CHAIN_BINARY
+cd ..
+
+curl -L https://raw.githubusercontent.com/hyphacoop/cosmos-ansible/main/examples/validator-keys/validator-40/priv_validator_key.json > $HOME_1/config/priv_validator_key.json
+pubkey=$(jq -r .pub_key.value $HOME_1/config/priv_validator_key.json)
+privkey=$(jq -r .priv_key.value $HOME_1/config/priv_validator_key.json)
+
+echo $MNEMONIC_1 | $CHAIN_BINARY keys add $MONIKER_1 --keyring-backend test --home $HOME_1 --recover
+
+echo "Setting up services..."
+echo "Creating script for $CHAIN_BINARY"
+echo "while true; do $HOME/go/bin/$CHAIN_BINARY testnet unsafe-start-local-validator --validator-operator '$VALOPER_1' --validator-pukey '$pubkey' --accounts-to-fund '$WALLET_1' --validator-privkey '$privkey' --home '$HOME_1'; sleep 1; done" > $HOME/$PROVIDER_SERVICE_1.sh
+chmod +x $HOME/$PROVIDER_SERVICE_1.sh
+
+# Run service in screen session
+if [ ! -d $HOME/artifact ]
+then
+    mkdir $HOME/artifact
+fi
+
+echo "Starting $CHAIN_BINARY"
+screen -L -Logfile $HOME/artifact/$PROVIDER_SERVICE_1.log -S $PROVIDER_SERVICE_1 -d -m bash $HOME/$PROVIDER_SERVICE_1.sh
+# set screen to flush log to 0
+screen -r $PROVIDER_SERVICE_1 -p0 -X logfile flush 0
+
+# Delegate some more stake. Voting fails otherwise, even though we have stake.
+$HOME/go/bin/$CHAIN_BINARY tx staking delegate $VALOPER_1 2000000uatom --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT -y --home $HOME_1 -o json
---
+++ scripts/stateful/software_upgrade.sh
@@ -0,0 +1,81 @@
+#!/bin/bash 
+# Test a gaia software upgrade via governance proposal.
+# It assumes gaia is running on the local host.
+
+gaia_host=$1
+gaia_port=$2
+upgrade_name=$3
+
+echo "Attempting upgrade to $upgrade_name."
+
+# Set time to wait for proposal to pass
+voting_period_seconds=20 # This is hardcoded by the test validator
+echo "Using ($voting_period_seconds)s voting period to calculate the upgrade height."
+    
+# Calculate upgrade height
+echo "Calculate upgrade height"
+block_time=1
+let voting_blocks_delta=$voting_period_seconds/$block_time+3
+height=$(curl -s http://$gaia_host:$gaia_port/block | jq -r .result.block.header.height)
+upgrade_height=$(($height+$voting_blocks_delta))
+echo "Upgrade block height set to $upgrade_height."
+
+upgrade_info="{\"binaries\":{\"linux/amd64\":\"$DOWNLOAD_URL\"}}"
+# Auto download: Set the binary paths need for the proposal message
+# Set up metadata
+jq -r --arg NAME "$upgrade_name" '.messages[0].plan.name |= $NAME' templates/proposal-software-upgrade.json > upgrade-1.json
+jq -r --arg HEIGHT "$upgrade_height" '.messages[0].plan.height |= $HEIGHT' upgrade-1.json > upgrade-2.json
+jq -r --arg INFO "$upgrade_info" '.messages[0].plan.info |= $INFO' upgrade-2.json > upgrade-3.json
+jq '.' upgrade-3.json
+proposal="$CHAIN_BINARY --output json tx gov submit-proposal upgrade-3.json --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT --yes --home $HOME_1"
+
+# Submit the proposal
+echo "Submitting the upgrade proposal."
+echo $proposal
+txhash=$($proposal | jq -r .txhash)
+sleep $(($COMMIT_TIMEOUT+2))
+
+# Get proposal ID from txhash
+echo "Getting proposal ID from txhash..."
+proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+echo "Proposal ID: $proposal_id"
+
+# Vote yes on the proposal
+echo "Submitting the \"yes\" vote to proposal $proposal_id..."
+vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT -y --home $HOME_1 -o json"
+echo $vote
+txhash=$($vote | jq -r .txhash)
+sleep $(($COMMIT_TIMEOUT+2))
+$CHAIN_BINARY q tx $txhash --home $HOME_1
+
+# Wait for the voting period to be over
+echo "Waiting for the voting period to end..."
+sleep $VOTING_PERIOD
+
+echo "Upgrade proposal $proposal_id status:"
+$CHAIN_BINARY q gov proposal $proposal_id --output json --home $HOME_1 | jq '.status'
+
+current_height=$(curl -s http://$gaia_host:$gaia_port/block | jq -r '.result.block.header.height')
+blocks_delta=$(($upgrade_height-$current_height))
+
+# Wait until the right height is reached
+echo "Waiting for the upgrade to take place at block height $upgrade_height..."
+tests/test_block_production.sh $gaia_host $gaia_port $blocks_delta
+echo "The upgrade height was reached."
+sleep $(($COMMIT_TIMEOUT*3))
+# Replace binary
+killall $CHAIN_BINARY
+screen -XS $PROVIDER_SERVICE_1 quit
+sleep 5
+tail -n 50 $HOME/artifact/$PROVIDER_SERVICE_1.log
+
+wget $DOWNLOAD_URL -O ./upgraded -q
+chmod +x ./upgraded
+mv ./upgraded $HOME/go/bin/$CHAIN_BINARY
+
+echo "Starting $CHAIN_BINARY"
+screen -L -Logfile $HOME/artifact/$PROVIDER_SERVICE_1.log -S $PROVIDER_SERVICE_1 -d -m bash $HOME/$PROVIDER_SERVICE_1.sh
+# set screen to flush log to 0
+screen -r $PROVIDER_SERVICE_1 -p0 -X logfile flush 0
+
+sleep 10
