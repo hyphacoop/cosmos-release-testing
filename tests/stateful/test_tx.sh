d4728a39392e6bf73e9b0b6d7611d9291802bd0c
ASoTNetworks <dev@aseriesoftubez.com>
2024-05-28 20:33:16 -0400

    wait longer

--- tests/stateful/test_tx.sh
+++ tests/stateful/test_tx.sh
@@ -45,7 +45,7 @@ check_code $TXHASH
 
 # Wait for rewards to accumulate
 echo "Waiting for rewards to accumulate"
-sleep 300
+sleep 600
 echo "Withdrawing rewards for test account..."
 starting_balance=$($CHAIN_BINARY q bank balances $WALLET_2 --home $HOME_1 -o json | jq -r '.balances[] | select(.denom=="uatom").amount')
 echo "Starting balance: $starting_balance"
