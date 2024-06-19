bf4b1561f34993eb3e016b5824d18fa03fc52bea
ASoTNetworks <dev@aseriesoftubez.com>
2024-06-19 12:52:01 -0400

    update get_block_size.sh

--- scripts/get_block_size.sh
+++ scripts/get_block_size.sh
@@ -3,12 +3,12 @@
 count=0
 while [ $count -lt $1 ]
 do
-current_height=$(curl -s 127.0.0.1:26660/metrics | \
-grep "cometbft_consensus_height{chain_id=\"provider\"}" | \
+current_height=$(curl -s 127.0.0.1:$VAL1_PROM_PORT/metrics | \
+grep "cometbft_consensus_height{chain_id=\"$CHAIN_ID\"}" | \
 awk -F " " '{ print $2 }' | awk -F"E" 'BEGIN{OFMT="%10.10f"} {print $1 * (10 ^ $2)}')
 
-current_size=$(curl -s 127.0.0.1:26660/metrics | \
-grep "cometbft_consensus_block_size_bytes{chain_id=\"provider\"}" | \
+current_size=$(curl -s 127.0.0.1:$VAL1_PROM_PORT/metrics | \
+grep "cometbft_consensus_block_size_bytes{chain_id=\"$CHAIN_ID\"}" | \
 awk -F " " '{ print $2 }' | awk -F"E" 'BEGIN{OFMT="%10.10f"} {print $1 * ((10 ^ $2)/1024/1024)}')
 echo "Current block: $current_height size is $current_size MB"
 let count=$count+1
