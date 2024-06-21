60206dd76748a4663aa5ab0eb8a91a654b826cb9
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-06-21 16:26:48 -0400

    test wasm results

--- tests/gaia-v18/test_cosmwasm.sh
+++ tests/gaia-v18/test_cosmwasm.sh
@@ -3,6 +3,9 @@
 INIT='{"count":100}'
 QUERY='{"get_count":{}}'
 EXEC="{\"increment\": {}}"
+
+$CHAIN_BINARY tx wasm store tests/gaia-v18/contract.wasm --from $WALLET_1 --chain-id $CHAIN_ID --gas 20000000 --gas-prices 0.005$DENOM --home $HOME_1 -o json | jq '.'
+
 txhash=$($CHAIN_BINARY tx wasm submit-proposal store-instantiate \
     tests/gaia-v18/contract.wasm $INIT \
     --label "my first contract" \
@@ -10,10 +13,10 @@ txhash=$($CHAIN_BINARY tx wasm submit-proposal store-instantiate \
     --instantiate-nobody true \
     --title "Store and instantiate CW template" \
     --summary "This proposal will store and instantiate the cw template contract" \
+    --deposit 10000000$DENOM -y \
     --from $WALLET_1 \
     --chain-id $CHAIN_ID \
     --gas 20000000 --gas-prices 0.005$DENOM \
-    --deposit 10000000$DENOM -y \
     --home $HOME_1 -o json | jq -r '.txhash')
 echo "Submitting the store-instantiate proposal..."
 # echo $proposal
@@ -44,12 +47,24 @@ echo "Contract address: $contract_address"
 count=$($CHAIN_BINARY q wasm contract-state smart $contract_address $QUERY --home $HOME_1 -o json | jq '.data.count')
 echo "Count: $count"
 
+if [[ "$count" == "100" ]]; then
+    echo "PASS: Contract was instantiated."
+else
+    echo "FAIL: Contract was not instantiated."
+    exit 1
+fi
+
 txhash=$($CHAIN_BINARY tx wasm execute $contract_address '{"increment":{}}' --from $WALLET_1 --chain-id $CHAIN_ID --gas auto --gas-adjustment 5 --gas-prices 0.005$DENOM -y --home $HOME_1 -o json | jq -r '.txhash')
 echo "Execute tx hash: $txhash"
 sleep $(($COMMIT_TIMEOUT*2))
 $CHAIN_BINARY q tx $txhash --home $HOME_1 -o json | jq '.'
 
-
 # Query
 count=$($CHAIN_BINARY q wasm contract-state smart $contract_address $QUERY --home $HOME_1 -o json | jq '.data.count')
 echo "Count: $count"
+if [[ "$count" == "101" ]]; then
+    echo "PASS: Contract was executed."
+else
+    echo "FAIL: Contract was not executed."
+    exit 1
+fi
