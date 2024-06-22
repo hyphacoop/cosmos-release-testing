e71cf2c05d4ae2ff901943d040eced9beffabca7
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-06-21 21:02:52 -0400

    timeout var

--- scripts/gaia-v18/set_expedited_period.sh
+++ scripts/gaia-v18/set_expedited_period.sh
@@ -6,15 +6,15 @@ proposal="$CHAIN_BINARY tx gov submit-proposal templates/gaia-v18/expedited-peri
 txhash=$($proposal | jq -r .txhash)
 echo "tx hash: $txhash" 
 
-sleep $TIMEOUT_COMMIT
-sleep $TIMEOUT_COMMIT
+sleep $COMMIT_TIMEOUT
+sleep $COMMIT_TIMEOUT
 echo "Getting proposal ID from txhash..."
 proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
 echo "Proposal ID: $proposal_id"
 
 printf "\nVOTING\n"
 $CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT --yes --home $HOME_1
-sleep $TIMEOUT_COMMIT
+sleep $COMMIT_TIMEOUT
 
 # exit 0
 echo "Waiting $VOTING_PERIOD seconds for param change proposal to pass..."
