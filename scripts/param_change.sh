2620cd9d08ca13571289b1b7ea4ccc3cc28345e2
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-07-23 10:15:43 -0400

    jailed query

--- scripts/param_change.sh
+++ scripts/param_change.sh
@@ -10,7 +10,11 @@ echo $proposal
 txhash=$($proposal | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
 echo "Proposal hash: $txhash"
-proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+if [ $COSMOS_SDK != "v50" ]; then
+    proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.logs[].events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+else
+    proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+fi
 vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json"
 txhash=$($vote | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
