6f41557dcc5a4619fc2beaa2fe76f24458abded1
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-07-26 20:44:26 -0400

    wait longer after hermes restart

--- scripts/gaia-v19/setup_connection_ica_controller.sh
+++ scripts/gaia-v19/setup_connection_ica_controller.sh
@@ -9,4 +9,5 @@ hermes create client --reference-chain $CHAIN_1 --host-chain $CHAIN_2
 hermes create connection --a-chain $CHAIN_1 --a-client 07-tendermint-$client_count --b-client 07-tendermint-0
 
 sudo systemctl restart hermes
-sleep 5
+sleep 30
+journalctl -u hermes | tail -n 10
