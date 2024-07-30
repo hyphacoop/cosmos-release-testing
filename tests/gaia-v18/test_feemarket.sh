7e20986af188cdb4d24341aa10c0e6e691b6249d
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-07-30 11:54:37 -0400

    bump large gas txs

--- tests/gaia-v18/test_feemarket.sh
+++ tests/gaia-v18/test_feemarket.sh
@@ -8,19 +8,19 @@ sudo systemctl restart $PROVIDER_SERVICE_1 --now
 echo "Restarting node after setting timeout_commit to 1m..."
 sleep 70s
 echo "Loading mempool with large gas txs..."
-scripts/gaia-v18/load_blocks.sh 50000000 30 0.005
+scripts/gaia-v18/load_blocks.sh 50000000 100 0.005
 echo "Loading mempool with txs..."
 scripts/gaia-v18/load_blocks.sh 100000 5000 0.01
 echo "Waiting for blocks to get loaded..."
 num_unconfirmed_txs=$(curl -s http://localhost:$VAL1_RPC_PORT/num_unconfirmed_txs | jq -r '.result.n_txs')
 echo "Unconfirmed txs: $num_unconfirmed_txs"
-while [ $num_unconfirmed_txs -gt "1000" ] ; do
+while [ $num_unconfirmed_txs -gt "2000" ] ; do
     echo "Sleeping for a minute..."
     sleep 1m
     num_unconfirmed_txs=$(curl -s http://localhost:$VAL1_RPC_PORT/num_unconfirmed_txs | jq -r '.result.n_txs')
     echo "Unconfirmed txs: $num_unconfirmed_txs"
 done
-echo "Less than 1000 txs remain in the mempool"
+echo "Less than 2000 txs remain in the mempool"
 
 current_price=$($CHAIN_BINARY q feemarket gas-prices --home $HOME_1 -o json | jq -r '.prices[0].amount')
 echo "Current gas price: $current_price$DENOM"
