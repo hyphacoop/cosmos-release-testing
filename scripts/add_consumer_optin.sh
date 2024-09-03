8a36787576ae5bd836d3c24fa9f55283534632dc
Dante <dante.a.sanchez@gmail.com>
2024-09-02 20:46:39 -0400

    do not set power shaping at first

--- scripts/add_consumer_optin.sh
+++ scripts/add_consumer_optin.sh
@@ -4,12 +4,12 @@ echo "Patching add template with spawn time..."
 spawn_time=$(date -u --iso-8601=ns -d '30 secs' | sed s/+00:00/Z/ | sed s/,/./) # 30 seconds in the future
 jq -r --arg SPAWNTIME "$spawn_time" '.initialization_parameters.spawn_time |= $SPAWNTIME' templates/create-consumer.json > create-spawn.json
 
-if [ $PSS_ENABLED == true ]; then
-    echo "Patching for PSS..."
-    jq -r --argjson TOPN $TOPN '.power_shaping_parameters.top_N |= $TOPN' create-spawn.json > create-topn.json
-    cp create-topn.json create-spawn.json
-    cat create-spawn.json
-fi
+# if [ $PSS_ENABLED == true ]; then
+#     echo "Patching for PSS..."
+#     jq -r --argjson TOPN $TOPN '.power_shaping_parameters.top_N |= $TOPN' create-spawn.json > create-topn.json
+#     cp create-topn.json create-spawn.json
+#     cat create-spawn.json
+# fi
 
 sed "s%\"chain_id\": \"\"%\"chain_id\": \"$CONSUMER_CHAIN_ID\"%g" create-spawn.json > create-$CONSUMER_CHAIN_ID.json
 rm create-spawn.json
