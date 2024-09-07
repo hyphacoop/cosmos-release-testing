b4aec047b949a9612d8a15240dd839cb39dd5acd
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-09-06 21:33:52 -0400

    print balances

--- tests/test_ica_send.sh
+++ tests/test_ica_send.sh
@@ -20,17 +20,21 @@ $CHAIN_BINARY_SECONDARY q bank balances $RECIPIENT --home $HOST_HOME -o json | j
 
 jq -r --arg FROMADDRESS "$ica_address" '.from_address = $FROMADDRESS' templates/ica-msg-send.json > msg-from.json
 jq -r --arg TOADDRESS "$RECIPIENT" '.from_address = $TOADDRESS' msg-from.json > msg.json
+echo "ICA Message:"
+jq '.' msg.json
 $CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat msg.json)" --encoding proto3 > send_packet.json
 echo "Sending ICA tx..."
 $CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 send_packet.json --from $CONTROLLER_ACCT --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json --home $CONTROLLER_HOME
 sleep 60
+
 
 ica_balance=$($CHAIN_BINARY_SECONDARY q bank balances $ica_address --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
 recipient_balance=$($CHAIN_BINARY_SECONDARY q bank balances $RECIPIENT --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
 
 echo "ICA balance in secondary chain: $ica_balance"
 echo "ICA tx recipient in secondary chain: $recipient_balance"
-
+echo "Full balance query response:"
+$CHAIN_BINARY_SECONDARY q bank balances $RECIPIENT --home $HOST_HOME -o json | jq -r '.'
 if [ $recipient_balance == "5555555" ]; then
     echo "PASS: Account in host chain received funds from ICA."
 else
