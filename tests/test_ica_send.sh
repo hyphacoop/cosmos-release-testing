a65443c7b837e02e166b793752e830b156c2053d
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-09-06 21:26:57 -0400

    test ICA send

--- tests/test_ica_send.sh
+++ tests/test_ica_send.sh
@@ -10,7 +10,7 @@
 # $CHAIN_BINARY_SECONDARY tx bank send $WALLET_1 $ica_address 100000000$DENOM --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json --home $HOST_HOME
 # sleep 10
 
-ica_address=$($CHAIN_BINARY q interchain-accounts controller interchain-account $WALLET_1 connection-0 --home $CONTROLLER_HOME -o json | jq -r '.address')
+ica_address=$($CHAIN_BINARY q interchain-accounts controller interchain-account $CONTROLLER_ACCT connection-0 --home $CONTROLLER_HOME -o json | jq -r '.address')
 echo "ICA address: $ica_address"
 
 echo "ICA balance in secondary chain:"
@@ -18,23 +18,22 @@ $CHAIN_BINARY_SECONDARY q bank balances $ica_address --home $HOST_HOME -o json |
 echo "ICA tx recipient in secondary chain:"
 $CHAIN_BINARY_SECONDARY q bank balances $RECIPIENT --home $HOST_HOME -o json | jq '.'
 
-# jq -r --arg ADDRESS "$ica_address" '.from_address = $ADDRESS' templates/ica-msg-send.json > msg.json
-# $CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat msg.json)" --encoding proto3 > send_packet.json
-# echo "Sending ICA tx..."
-# $CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 send_packet.json --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json --home $CONTROLLER_HOME
-# sleep 60
+jq -r --arg FROMADDRESS "$ica_address" '.from_address = $FROMADDRESS' templates/ica-msg-send.json > msg-from.json
+jq -r --arg TOADDRESS "$RECIPIENT" '.from_address = $TOADDRESS' msg-from.json > msg.json
+$CHAIN_BINARY tx interchain-accounts host generate-packet-data "$(cat msg.json)" --encoding proto3 > send_packet.json
+echo "Sending ICA tx..."
+$CHAIN_BINARY tx interchain-accounts controller send-tx connection-0 send_packet.json --from $CONTROLLER_ACCT --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json --home $CONTROLLER_HOME
+sleep 60
 
-# ica_balance=$($CHAIN_BINARY_SECONDARY q bank balances $ica_address --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
-# recipient_balance=$($CHAIN_BINARY_SECONDARY q bank balances $WALLET_2 --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
+ica_balance=$($CHAIN_BINARY_SECONDARY q bank balances $ica_address --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
+recipient_balance=$($CHAIN_BINARY_SECONDARY q bank balances $RECIPIENT --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
 
-# echo "ICA balance in secondary chain: $ica_balance"
-# echo "ICA tx recipient in secondary chain: $recipient_balance"
+echo "ICA balance in secondary chain: $ica_balance"
+echo "ICA tx recipient in secondary chain: $recipient_balance"
 
-# recipient_balance=$($CHAIN_BINARY_SECONDARY q bank balances $WALLET_2 --home $HOST_HOME -o json | jq -r '.balances[] | select(.denom == "uatom").amount')
-
-# if [ $recipient_balance == "5555555" ]; then
-#     echo "PASS: Account in host chain received funds from ICA."
-# else
-#     echo "FAIL: Account in host chain did not receive funds from ICA."
-#     exit 1
-# fi
+if [ $recipient_balance == "5555555" ]; then
+    echo "PASS: Account in host chain received funds from ICA."
+else
+    echo "FAIL: Account in host chain did not receive funds from ICA."
+    exit 1
+fi
