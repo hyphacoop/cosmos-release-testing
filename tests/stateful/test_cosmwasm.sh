c0d5c02b3bad0e747b57c32f4530ef423d8ce03a
ASoTNetworks <dev@aseriesoftubez.com>
2024-09-14 22:40:39 -0400

    fix command

--- tests/stateful/test_cosmwasm.sh
+++ tests/stateful/test_cosmwasm.sh
@@ -5,9 +5,11 @@ INIT='{"count":100}'
 QUERY='{"get_count":{}}'
 EXEC="{\"increment\": {}}"
 
+echo "[INFO]: Store contract.wasm"
 $CHAIN_BINARY tx wasm store tests/gaia-v18/contract.wasm --from $WALLET_1 --chain-id $CHAIN_ID --gas 20000000 --gas-prices 0.005$DENOM --home $HOME_1 -y
 tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 
+echo "[INFO]:Submitting the store-instantiate proposal..."
 proposal=$($CHAIN_BINARY tx wasm submit-proposal store-instantiate \
     tests/gaia-v18/contract.wasm $INIT \
     --label "my first contract" \
@@ -20,39 +22,39 @@ proposal=$($CHAIN_BINARY tx wasm submit-proposal store-instantiate \
     --chain-id $CHAIN_ID \
     --gas 20000000 --gas-prices 0.005$DENOM \
     --home $HOME_1 -o json)
-echo "Submitting the store-instantiate proposal..."
-echo $proposal | jq -r '.'
-txhash=$($proposal | jq -r .txhash)
 tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
+echo "[DEBUG]: submit json output:"
+echo $proposal | jq -r '.'
+txhash=$(echo $proposal | jq -r .txhash)
 
-echo "Getting proposal ID from txhash..."
+echo "[INFO]: Getting proposal ID from txhash..."
 proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
-echo "Proposal ID: $proposal_id"
+echo "[INFO]: Proposal ID: $proposal_id"
 
-echo "Submitting the \"yes\" vote to proposal $proposal_id..."
+echo "[INFO]: Submitting the \"yes\" vote to proposal $proposal_id..."
 vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE$DENOM --gas-adjustment $GAS_ADJUSTMENT -y --home $HOME_1 -o json"
 echo $vote
 txhash=$($vote | jq -r .txhash)
 tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
 $CHAIN_BINARY q tx $txhash --home $HOME_1
 
-echo "Waiting for the voting period to end..."
+echo "[INFO]: Waiting for the voting period to end..."
 sleep $VOTING_PERIOD
 
 # Use code 1
 # Get contract address
 code_id=1
 contract_address=$($CHAIN_BINARY q wasm list-contract-by-code $code_id --home $HOME_1 -o json | jq -r '.contracts[0]')
-echo "Contract address: $contract_address"
+echo "[INFO]: Contract address: $contract_address"
 
 # Query
 count=$($CHAIN_BINARY q wasm contract-state smart $contract_address $QUERY --home $HOME_1 -o json | jq '.data.count')
-echo "Count: $count"
+echo "[INFO]: Count: $count"
 
 if [[ "$count" == "100" ]]; then
-    echo "PASS: Contract was instantiated."
+    echo "[PASS]: Contract was instantiated."
 else
-    echo "FAIL: Contract was not instantiated."
+    echo "[FAIL]: Contract was not instantiated."
     exit 1
 fi
 
