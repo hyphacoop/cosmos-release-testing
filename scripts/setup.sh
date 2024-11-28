af289eb8dfe23ffa110d8096bcee31eb1cba3d3a
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-11-27 19:57:25 -0500

    closer stake

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -159,7 +159,7 @@ do
     toml set --toml-path ${homes[i]}/config/config.toml p2p.laddr "tcp://0.0.0.0:${p2p_ports[i]}"
     toml set --toml-path ${homes[i]}/config/config.toml p2p.allow_duplicate_ip true
     toml set --toml-path ${homes[i]}/config/config.toml block_sync false
-    toml set --toml-path ${homes[i]}/config/config.toml consensus.timeout_commit "$TIMEOUT_COMMITS"
+    toml set --toml-path ${homes[i]}/config/config.toml consensus.timeout_commit "${TIMEOUT_COMMIT}s"
     toml set --toml-path ${homes[i]}/config/config.toml p2p.persistent_peers ""
     if [ $i -ne "0" ]; then
         toml set --toml-path ${homes[i]}/config/config.toml p2p.persistent_peers "$val1_peer"
