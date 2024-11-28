e7e92e70e35d617bbcb4a3f5b8f258aea4e68217
Dante Sanchez <dante.a.sanchez@gmail.com>
2024-11-27 19:47:26 -0500

    check signatures

---
+++ scripts/check_signatures.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# source vars.sh
+
+monikers=()
+homes=()
+api_ports=()
+rpc_ports=()
+for i in $(seq -w 001 $validator_count)
+do
+    moniker=$moniker_prefix$i
+    monikers+=($moniker)
+    home=$home_prefix$i
+    homes+=($home)
+    api_port=$api_prefix$i
+    api_ports+=($api_port)
+    rpc_port=$rpc_prefix$i
+    rpc_ports+=($rpc_port)
+done
+
+signature_count=$(curl -s http://localhost:${rpc_ports[0]}/block | jq -r '[.result.block.last_commit.signatures[] | select(.block_id_flag==2)] | length'
+echo "> Signature count: $signature_count"
+if [ "$signature_count" = "$validator_count" ]; then
+    echo "> All validators are signing."
+    exit 0
+else
+    echo "> Not all validators are signing."
+    exit 1
+fi
