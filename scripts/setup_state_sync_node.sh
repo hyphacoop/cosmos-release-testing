bbea634fabd19c7fb4ce5c9c8667193c7c9cb818
Dante <dante.a.sanchez@gmail.com>
2024-12-18 10:30:35 -0500

    enable state sync

---
+++ scripts/setup_state_sync_node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Configure variables before running this file.
+# source vars.sh
+
+echo "> Creating arrays"
+
+homes=()
+for i in $(seq -w 001 $validator_count)
+do
+    home=$home_prefix$i
+    homes+=($home)
+    log=$log_prefix$i
+    logs+=($log)
+done
+
+echo "> Enabling state sync"
+toml set --toml-path ${homes[-1]}/config/app.toml state-sync.snapshot-interval 50
+
+
+echo "> Restarting nodes."
+./stop.sh
+./start.sh
+sleep 10
+tail -n 100 ${logs-1}
