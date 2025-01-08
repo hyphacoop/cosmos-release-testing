b703abc4e697a679e7d00cdd90da56041515e8b7
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-01-08 11:02:11 -0500

    launch v5.2.0 pre-upgrade

--- scripts/setup_relayer.sh
+++ scripts/setup_relayer.sh
@@ -25,6 +25,7 @@ if [ $RELAYER == "hermes" ]; then
     hermes keys add --chain v400-one --mnemonic-file mnemonic.txt
     hermes keys add --chain v450-one --mnemonic-file mnemonic.txt
     hermes keys add --chain v450-two --mnemonic-file mnemonic.txt
+    hermes keys add --chain v520-one --mnemonic-file mnemonic.txt
     hermes keys add --chain v630-one --mnemonic-file mnemonic.txt
     hermes keys add --chain v630-two --mnemonic-file mnemonic.txt
     hermes keys add --chain v640-one --mnemonic-file mnemonic.txt
