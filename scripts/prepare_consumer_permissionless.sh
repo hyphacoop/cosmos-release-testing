63d402ba2591817fb7fcfe65b64aafc747eb93dd
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-01-08 11:24:12 -0500

    transform genesis for v5.2.0

--- scripts/prepare_consumer_permissionless.sh
+++ scripts/prepare_consumer_permissionless.sh
@@ -64,6 +64,24 @@ if [ "$CONSUMER_ICS" == "v4.0.0" ]; then
     # fi
 fi
 
+if [ "$CONSUMER_ICS" == "v5.2.0" ]; then
+    if [ "$PROVIDER_ICS" == "v6.3.0" ]; then
+        echo "Patching for ICS compatibility: Provider $PROVIDER_ICS | Consumer $CONSUMER_ICS"
+        $ICS_TRANSFORM_BINARY genesis transform ccv.json > ccv-520-1.json
+        cp ccv-520-1.json ccv.json
+    fi
+    # elif [ "$PROVIDER_ICS" == "v4.1.0" ]; then
+    #     echo "Patching for ICS compatibility: Provider $PROVIDER_ICS | Consumer $CONSUMER_ICS"
+    #     $ICS_TRANSFORM_BINARY genesis transform --to v3.3.x ccv.json > ccv-330-1.json
+    #     cp ccv-330-1.json ccv.json
+    # fi
+    # elif [ "$PROVIDER_ICS" == "v4.1.1" ]; then
+    #     echo "Patching for ICS compatibility: Provider $PROVIDER_ICS | Consumer $CONSUMER_ICS"
+    #     $ICS_TRANSFORM_BINARY genesis transform --to v3.3.x ccv.json > ccv-330-1.json
+    #     cp ccv-330-1.json ccv.json
+    # fi
+fi
+
 if [ "$CONSUMER_ICS" == "v6.3.0" ]; then
     if [ "$PROVIDER_ICS" == "v6.4.0" ]; then
         echo "Patching for ICS compatibility: Provider $PROVIDER_ICS | Consumer $CONSUMER_ICS"
