3bc0571d9f3af97b25900d748e49944556d63d77
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-02-10 12:14:54 -0500

    distribute stake

--- tests/test_block_signatures.sh
+++ tests/test_block_signatures.sh
@@ -6,10 +6,12 @@ port=$2
 expected=$3
 
 # Check how many validators are signing blocks
-signature_count=$(curl -s http://$host:$port/block | jq '[.result.block.last_commit.signatures[] | select(.block_id_flag==2)] | length')
+signatures=$(curl -s http://$host:$port/block | jq '[.result.block.last_commit.signatures[]'
+signature_count=$(echo $signatures | jq 'select(.block_id_flag==2)] | length')
 echo "> Signature count: $signature_count"
 if (( "$signature_count" != "$3" )); then
     echo "> Not all validators are signing blocks."
+    echo "$signatures"
     exit 1
 else
     echo "> All validators are signing blocks."
