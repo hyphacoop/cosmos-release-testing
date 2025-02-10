e1607d0cab7ad077fbf55275bcd96e66a1a9e534
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-02-10 12:16:26 -0500

    jq syntax

--- tests/test_block_signatures.sh
+++ tests/test_block_signatures.sh
@@ -6,7 +6,7 @@ port=$2
 expected=$3
 
 # Check how many validators are signing blocks
-signatures=$(curl -s http://$host:$port/block | jq '[.result.block.last_commit.signatures[]'
+signatures=$(curl -s http://$host:$port/block | jq '[.result.block.last_commit.signatures[]')
 signature_count=$(echo $signatures | jq 'select(.block_id_flag==2)] | length')
 echo "> Signature count: $signature_count"
 if (( "$signature_count" != "$3" )); then
