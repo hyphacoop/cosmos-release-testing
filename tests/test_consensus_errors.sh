80cdbf5c0d34431c9a01a552186518796b029c24
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-02-12 12:00:21 -0500

    consensus check

---
+++ tests/test_consensus_errors.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# Check the logs are clear of wrong apphash errors.
+APPHASH_ERROR='wrong Block.Header.AppHash'
+LOOKBACK=50
+apphash=$(journalctl -u $PROVIDER_SERVICE_1 | grep -c "$APPHASH_ERROR" || true)
+if [ $apphash != "0" ]; then
+  echo "AppHash error found!"
+  journalctl -u $PROVIDER_SERVICE_1 | tail -n $LOOKBACK
+  exit 1
+fi
+apphash=$(journalctl -u $PROVIDER_SERVICE_2 | grep -c "$APPHASH_ERROR" || true)
+if [ $apphash != "0" ]; then
+  echo "AppHash error found!"
+  journalctl -u $PROVIDER_SERVICE_2 | tail -n $LOOKBACK
+  exit 1
+fi
+apphash=$(journalctl -u $PROVIDER_SERVICE_3 | grep -c "$APPHASH_ERROR" || true)
+if [ $apphash != "0" ]; then
+  echo "AppHash error found!"
+  journalctl -u $PROVIDER_SERVICE_3 | tail -n $LOOKBACK
+  exit 1
+fi
