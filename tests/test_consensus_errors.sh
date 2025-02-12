4b9495b30288178eb8469b96d30798b55d42ef1c
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-02-12 16:31:19 -0500

    don't skip counter contract

--- tests/test_consensus_errors.sh
+++ tests/test_consensus_errors.sh
@@ -4,19 +4,19 @@ APPHASH_ERROR='consensus deems this block invalid'
 LOOKBACK=50
 apphash=$(journalctl -u $PROVIDER_SERVICE_1 | grep -c "$APPHASH_ERROR" || true)
 if [ $apphash != "0" ]; then
-  echo "AppHash error found!"
+  echo "AppHash error found in val1!"
   journalctl -u $PROVIDER_SERVICE_1 | tail -n $LOOKBACK
   exit 1
 fi
 apphash=$(journalctl -u $PROVIDER_SERVICE_2 | grep -c "$APPHASH_ERROR" || true)
 if [ $apphash != "0" ]; then
-  echo "AppHash error found!"
+  echo "AppHash error found in val2!"
   journalctl -u $PROVIDER_SERVICE_2 | tail -n $LOOKBACK
   exit 1
 fi
 apphash=$(journalctl -u $PROVIDER_SERVICE_3 | grep -c "$APPHASH_ERROR" || true)
 if [ $apphash != "0" ]; then
-  echo "AppHash error found!"
+  echo "AppHash error found in val3!"
   journalctl -u $PROVIDER_SERVICE_3 | tail -n $LOOKBACK
   exit 1
 fi
