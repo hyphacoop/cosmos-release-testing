40b92c5951d40daf8a45efb680c0f3576829c329
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-02-28 11:07:20 -0500

    v22.3.0 binary swap

--- scripts/generate_upgrade_matrix_fresh.py
+++ scripts/generate_upgrade_matrix_fresh.py
@@ -16,55 +16,55 @@ RELAYER = args.relayer
 
 SKIP_TARGET_VERSIONS = ['v22.0.0-rc0', 'v22.0.0', 'v22.1.0']
 
-# # Must provide a cutoff version, e.g. 'v6.0.4'
-# starting_version = args.starting_version.split('.')
-# version_major = int(starting_version[0][1:])
-# version_minor = int(starting_version[1])
-# version_patch = int(starting_version[2])
+# Must provide a cutoff version, e.g. 'v6.0.4'
+starting_version = args.starting_version.split('.')
+version_major = int(starting_version[0][1:])
+version_minor = int(starting_version[1])
+version_patch = int(starting_version[2])
 
-# # Read JSON output from generate_version_matrix.py
-# with open('releases.json', 'r', encoding='utf-8') as releases_file:
-#     releases_list = json.load(releases_file)
+# Read JSON output from generate_version_matrix.py
+with open('releases.json', 'r', encoding='utf-8') as releases_file:
+    releases_list = json.load(releases_file)
 
-# # Filter names that do not comply to `vA.B.C` format (-rcX suffix is fine)
-# version_pattern = r'v\d+.\d+.\d+(-rc\d*)?'
-# release_names = [ release['name'] for release in releases_list if re.fullmatch(version_pattern, release['name']) ]
+# Filter names that do not comply to `vA.B.C` format (-rcX suffix is fine)
+version_pattern = r'v\d+.\d+.\d+(-rc\d*)?'
+release_names = [ release['name'] for release in releases_list if re.fullmatch(version_pattern, release['name']) ]
 
-# releases = []
-# rc_releases = []
-# for name in release_names:
-#     components = name.split('.')
-#     if ' ' in components[2]:
-#         continue
-#     # Trim list to only releases from specified version onwards
-#     if (int(components[0][1:]) == version_major and (int(components[1]) >= version_minor)) and \
-#        (int(components[0][1:]) == version_major and (int(components[2].split('-')[0]) >= version_patch)) or \
-#         int(components[0][1:]) > version_major:
-#         if 'rc' in components[2]:
-#             rc_releases.append(name)
-#         else:
-#             releases.append(name)
+releases = []
+rc_releases = []
+for name in release_names:
+    components = name.split('.')
+    if ' ' in components[2]:
+        continue
+    # Trim list to only releases from specified version onwards
+    if (int(components[0][1:]) == version_major and (int(components[1]) >= version_minor)) and \
+       (int(components[0][1:]) == version_major and (int(components[2].split('-')[0]) >= version_patch)) or \
+        int(components[0][1:]) > version_major:
+        if 'rc' in components[2]:
+            rc_releases.append(name)
+        else:
+            releases.append(name)
 
-# # Remove all rcs from the list if there is a final release available
-# for rc in rc_releases:
-#     components = rc.split('.')
-#     if int(components[0][1:]) > version_major:
-#         releases.append(rc)
+# Remove all rcs from the list if there is a final release available
+for rc in rc_releases:
+    components = rc.split('.')
+    if int(components[0][1:]) > version_major:
+        releases.append(rc)
 
-# # Set upgrade versions to target for each release
-# # matrix = {release: [] for release in releases}
-# matrix = {}
-# for release in releases:
-#     rel_major_version = int(release.split('.')[0][1:])
-#     if rel_major_version == version_major:
-#         matrix[release] = []
+# Set upgrade versions to target for each release
+# matrix = {release: [] for release in releases}
+matrix = {}
+for release in releases:
+    rel_major_version = int(release.split('.')[0][1:])
+    if rel_major_version == version_major:
+        matrix[release] = []
 
-# for start_version, _ in matrix.items():
-#     matrix[start_version] = [
-#         release
-#         for release in releases
-#         if int(release.split('.')[0][1:]) > int(start_version.split('.')[0][1:])]
-matrix = {'v22.1.0': ['v22.2.0']}
+for start_version, _ in matrix.items():
+    matrix[start_version] = [
+        release
+        for release in releases
+        if int(release.split('.')[0][1:]) > int(start_version.split('.')[0][1:])]
+# matrix = {'v22.1.0': ['v22.2.0']}
 
 # Assemble matrix include section:
 includes = []
