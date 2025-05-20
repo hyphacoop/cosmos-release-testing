af2ca61a307864e2525bc93ff1575b438520e08d
ASoTNetworks <dev@aseriesoftubez.com>
2025-05-20 15:53:15 +0000

    get value from proposal script

--- scripts/submit_proposal.sh
+++ scripts/submit_proposal.sh
@@ -41,10 +41,39 @@ done
 # Get proposal ID from txhash
 echo "[INFO] Getting proposal ID from txhash..."
 $CHAIN_BINARY q tx $txhash --home $HOME_1
-proposal_id=$($CHAIN_BINARY q tx $txhash --home $HOME_1 --output json | jq -r '.events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
+proposal_tx_json=$($CHAIN_BINARY q tx $txhash --home $HOME_1 --output json)
+export PROPOSAL_TX_JSON=proposal_id_json
+proposal_id=$(echo $proposal_tx_json | jq -r '.events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
 
 echo "[INFO] Voting on proposal $proposal_id..."
-$CHAIN_BINARY tx gov vote $proposal_id $vote_option --gas auto --gas-prices $GAS_PRICES$DENOM --gas-adjustment $GAS_ADJUSTMENT --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -b sync -y
+vote="$CHAIN_BINARY tx gov vote $proposal_id $vote_option --gas auto --gas-prices $GAS_PRICES$DENOM --gas-adjustment $GAS_ADJUSTMENT --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -b sync -y -o json"
+echo "[INFO] tx output:"
+echo $vote
+gaiadout=$($vote)
+echo "[INFO] gaiad output:"
+echo "$gaiadout"
+txhash=$(echo "$gaiadout" | jq -r .txhash)
+
+# Wait for block to go on chain
+cur_height=$(curl -s http://127.0.0.1:$VAL1_RPC_PORT/block | jq -r .result.block.header.height)
+let to_height=$cur_height+1
+echo_height=1
+echo "[INFO] Current height: $cur_height"
+
+until [[ "${cur_height}" -gt "${to_height}" ]]
+do
+    cur_height=$(curl -s http://127.0.0.1:$VAL1_RPC_PORT/block | jq -r .result.block.header.height)
+    if [ $echo_height -ne $cur_height ]
+    then
+        echo "[INFO] Current height: $cur_height"
+        echo_height=$cur_height
+    fi
+    sleep 1
+done
+
+vote_tx_json=$($CHAIN_BINARY q tx $txhash --home $HOME_1 --output json)
+export VOTE_TX_JSON=vote_tx_json
+
 $CHAIN_BINARY q gov tally $proposal_id --home $HOME_1
 echo "[INFO] Waiting for proposal to pass..."
 sleep $VOTING_PERIOD
