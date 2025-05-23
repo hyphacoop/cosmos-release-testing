09acdda087a61bab9f96bf7434386a2b19b48797
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-05-23 11:53:13 -0400

    update gov params

--- scripts/param_change.sh
+++ scripts/param_change.sh
@@ -5,14 +5,11 @@ proposal_json=$1
 proposal="$CHAIN_BINARY tx gov submit-proposal $proposal_json --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y"
 
 echo $proposal
-# response=$($proposal)
-# echo $response
-
 txhash=$($proposal | jq -r .txhash)
 sleep $(( $COMMIT_TIMEOUT*2 ))
 
 echo "Proposal hash: $txhash"
-$CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.'
+#$CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.'
 proposal_id=$($CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.events[] | select(.type=="submit_proposal") | .attributes[] | select(.key=="proposal_id") | .value')
 
 vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --home $HOME_1 --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -y -o json"
--- tests/test_expedited_proposals.sh
+++ tests/test_expedited_proposals.sh
@@ -10,7 +10,7 @@ max_validators=50
 jq --argjson MAXVALS $max_validators '.messages[0].params.max_validators=$MAXVALS' templates/proposal-staking-params.json > staking-params.json
 jq '.' staking-params.json
 
-jq --argjson MAXVALS $max_validators '.expedited=true' staking-params.json > expedited.json
+jq '.expedited=true' staking-params.json > expedited.json
 jq '.' expedited.json
 
 echo "> Passing proposal."
@@ -28,3 +28,26 @@ fi
 
 echo ">Gov params:"
 $CHAIN_BINARY q gov params --output json --home $HOME_1 | jq '.'
+value_pre_change=$($CHAIN_BINARY q gov params --output json --home $HOME_1 | jq -r '.params.quorum')
+new_quorum=0.4
+
+params=$($CHAIN_BINARY q gov params --output json --home $HOME_1 | jq -r '.messages[0].params')
+jq --argjson PARAMS $params '.messages[0].params = $PARAMS' templates/proposal-gov-params.json > gov-params.json
+jq '.' gov-params.json
+jq --arg QUORUM "$new_quorum" '.messages[0].params.quorum=$QUORUM' gov-params.json > gov-params-quorum.json
+jq '.' gov-params-quorum.json
+jq '.expedited=true' gov-params-quorum.json > expedited.json
+jq '.' expedited.json
+
+echo "> Passing proposal."
+scripts/param_change.sh expedited.json
+echo "> Gov params:"
+$CHAIN_BINARY q gov params --output json --home $HOME_1 | jq '.'
+value_post_change=$($CHAIN_BINARY q gov params --output json --home $HOME_1 | jq -r '.params.quorum')
+echo "> Started at $value_pre_change, ended at $value_post_change"
+if [[ "$value_post_change" != "$value_pre_change" ]]; then
+    echo "PASS: Expedited proposal was executed."
+else
+    echo "FAIL: Expedited proposal was not executed."
+    exit 1
+fi
