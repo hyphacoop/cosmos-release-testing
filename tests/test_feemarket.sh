ce18e4397c226456348bea0a49a263b2e0beb234
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-05-30 15:37:58 -0400

    check gas price at specified height

--- tests/test_feemarket.sh
+++ tests/test_feemarket.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PAYLOAD_SIZE=50000
+PAYLOAD_SIZE=40000
 
 preload_price=$($CHAIN_BINARY q feemarket gas-prices --home $HOME_1 -o json | jq -r '.prices[0].amount')
 echo "Pre-load price: $preload_price$DENOM"
@@ -10,16 +10,23 @@ cat payload.txt
 jq --rawfile PAYLOAD payload.txt '$PAYLOAD'
 echo "> Assembling text proposal."
 jq --rawfile PAYLOAD payload.txt '.summary |= $PAYLOAD' templates/proposal-text.json > proposal.json
-echo "> Proposal JSON:"
-jq '.' proposal.json
+# echo "> Proposal JSON:"
+# jq '.' proposal.json
 echo "> Submitting proposal."
 txhash_1=$($CHAIN_BINARY tx gov submit-proposal proposal.json --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM --home $HOME_1 -y -o json | jq -r '.txhash')
 txhash_2=$($CHAIN_BINARY tx gov submit-proposal proposal.json --from $WALLET_2 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM --home $HOME_1 -y -o json | jq -r '.txhash')
 sleep $(($COMMIT_TIMEOUT+2))
 
 echo "> Proposal hashes:"
-$CHAIN_BINARY q tx $txhash_1 --home $HOME_1 -o json | jq '.'
-$CHAIN_BINARY q tx $txhash_2 --home $HOME_1 -o json | jq '.'
+height_1=$($CHAIN_BINARY q tx $txhash_1 --home $HOME_1 -o json | jq -r '.height')
+height_2=$($CHAIN_BINARY q tx $txhash_2 --home $HOME_1 -o json | jq -r '.height')
+
+echo "> Height for tx 1: $height_1"
+echo "> Base gas price at proposal height:"
+$CHAIN_BINARY q feemarket state --home $HOME_1 --height $height_1 -o json | jq -r '.base_gas_price'
+echo "> Base gas price at proposal height+1:"
+$CHAIN_BINARY q feemarket state --home $HOME_1 --height $(($height_1+1)) -o json | jq -r '.base_gas_price'
+
 
 current_price=$($CHAIN_BINARY q feemarket gas-prices --home $HOME_1 -o json | jq -r '.prices[0].amount')
 echo "Current gas price: $current_price$DENOM"
