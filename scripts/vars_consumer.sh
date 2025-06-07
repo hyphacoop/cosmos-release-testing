82df5d36e4af1cafa51e5466c8a32fef68c08e5e
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-06-07 00:10:05 -0400

    consumer gas price

--- scripts/vars_consumer.sh
+++ scripts/vars_consumer.sh
@@ -7,7 +7,7 @@ export CONSUMER_DENOM=ucon
 export CONSUMER_DOWNTIME_WINDOW=100000
 export RECIPIENT=cosmos1r5v5srda7xfth3hn2s26txvrcrntldjumt8mhl
 export CONSUMER_DENOM=ucon
-export CONSUMER_GAS_PRICE=0.005$DENOM
+export CONSUMER_GAS_PRICE=0.005$CONSUMER_DENOM
 
 export ICS_TRANSFORM_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v3.3.0-transform/interchain-security-cd
 export ICS_TRANSFORM_BINARY=cd-transform
