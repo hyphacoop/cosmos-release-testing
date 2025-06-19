0d7c5ec86b222afe2cd1d0125d1453bcefe33b62
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-06-19 12:02:53 -0400

    add v6.4.0

--- scripts/vars_consumer_v400_one.sh
+++ scripts/vars_consumer_v400_one.sh
@@ -12,6 +12,7 @@ export CONSUMER_GAS_PRICE=0.005$CONSUMER_DENOM
 
 export ICS_TRANSFORM_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v3.3.0-transform/interchain-security-cd
 export ICS_TRANSFORM_BINARY=$CONSUMER_CHAIN_BINARY
+export CONSUMER_ICS="v4.0.0"
 
 export consumer_moniker_prefix='con_'
 export consumer_home_prefix='/home/runner/.con_'
