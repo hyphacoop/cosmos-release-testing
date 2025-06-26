55a3731da169474559b388ec73f607d33939f5f5
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-06-26 14:17:07 -0400

    expedited proposals

--- tests/test_expedited_proposals.sh
+++ tests/test_expedited_proposals.sh
@@ -2,8 +2,8 @@
 
 
 echo "> Staking params:"
-$CHAIN_BINARY q staking params --output json --home $HOME_1 | jq '.'
-value_pre_change=$($CHAIN_BINARY q staking params --output json --home $HOME_1 | jq -r '.params.max_validators')
+$CHAIN_BINARY q staking params --output json --home $whale_home | jq '.'
+value_pre_change=$($CHAIN_BINARY q staking params --output json --home $whale_home | jq -r '.params.max_validators')
 echo "> Updating staking params via expedited proposal."
 
 max_validators=50
@@ -16,8 +16,8 @@ jq '.' expedited.json
 echo "> Passing proposal."
 scripts/param_change.sh expedited.json
 echo "> Staking params:"
-$CHAIN_BINARY q staking params --output json --home $HOME_1 | jq '.'
-value_post_change=$($CHAIN_BINARY q staking params --output json --home $HOME_1 | jq -r '.params.max_validators')
+$CHAIN_BINARY q staking params --output json --home $whale_home | jq '.'
+value_post_change=$($CHAIN_BINARY q staking params --output json --home $whale_home | jq -r '.params.max_validators')
 echo "> Started at $value_pre_change, ended at $value_post_change"
 if [[ "$value_post_change" != "$value_pre_change" ]]; then
     echo "PASS: Expedited proposal was executed."
@@ -27,11 +27,11 @@ else
 fi
 
 echo ">Gov params:"
-$CHAIN_BINARY q gov params --output json --home $HOME_1 | jq '.'
-value_pre_change=$($CHAIN_BINARY q gov params --output json --home $HOME_1 | jq -r '.params.quorum')
+$CHAIN_BINARY q gov params --output json --home $whale_home | jq '.'
+value_pre_change=$($CHAIN_BINARY q gov params --output json --home $whale_home | jq -r '.params.quorum')
 new_quorum=0.4
 
-$CHAIN_BINARY q gov params --output json --home $HOME_1 | jq '.params' >  params.json
+$CHAIN_BINARY q gov params --output json --home $whale_home | jq '.params' >  params.json
 echo "params.json:"
 cat params.json
 jq --slurpfile PARAMS params.json '.messages[0].params = $PARAMS[0]' templates/proposal-gov-params.json > gov-params.json
@@ -45,8 +45,8 @@ jq '.' expedited.json
 echo "> Passing proposal."
 scripts/param_change.sh expedited.json
 echo "> Gov params:"
-$CHAIN_BINARY q gov params --output json --home $HOME_1 | jq '.'
-value_post_change=$($CHAIN_BINARY q gov params --output json --home $HOME_1 | jq -r '.params.quorum')
+$CHAIN_BINARY q gov params --output json --home $whale_home | jq '.'
+value_post_change=$($CHAIN_BINARY q gov params --output json --home $whale_home | jq -r '.params.quorum')
 echo "> Started at $value_pre_change, ended at $value_post_change"
 if [[ "$value_post_change" != "$value_pre_change" ]]; then
     echo "PASS: Expedited proposal was executed."
