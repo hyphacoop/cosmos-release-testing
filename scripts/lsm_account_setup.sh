a2bc66f369686b33673188180f3456bb9b2c888f
Dante <dante.a.sanchez@gmail.com>
2025-06-27 14:14:16 -0400

    start chain

--- scripts/lsm_account_setup.sh
+++ scripts/lsm_account_setup.sh
@@ -17,8 +17,8 @@ happy_liquid_3=$($CHAIN_BINARY keys list --home $whale_home --output json | jq -
 happy_owner=$($CHAIN_BINARY keys list --home $whale_home --output json | jq -r '.[] | select(.name=="happy_owner").address')
 
 echo > "Funding bonding and tokenizing accounts."
-submit_tx "tx bank send $WALLET_1 $happy_bonding  $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $whale_home
-submit_tx "tx bank send $WALLET_1 $happy_liquid_1 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $whale_home
-submit_tx "tx bank send $WALLET_1 $happy_liquid_2 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $whale_home
-submit_tx "tx bank send $WALLET_1 $happy_liquid_3 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $whale_home
-submit_tx "tx bank send $WALLET_1 $happy_owner    $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM -o json -y" $CHAIN_BINARY $whale_home
+submit_tx "tx bank send $WALLET_1 $happy_bonding  $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE -o json -y" $CHAIN_BINARY $whale_home
+submit_tx "tx bank send $WALLET_1 $happy_liquid_1 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE -o json -y" $CHAIN_BINARY $whale_home
+submit_tx "tx bank send $WALLET_1 $happy_liquid_2 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE -o json -y" $CHAIN_BINARY $whale_home
+submit_tx "tx bank send $WALLET_1 $happy_liquid_3 $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE -o json -y" $CHAIN_BINARY $whale_home
+submit_tx "tx bank send $WALLET_1 $happy_owner    $funding$DENOM --from $WALLET_1 --gas auto --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE -o json -y" $CHAIN_BINARY $whale_home
