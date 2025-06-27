3acaab592e412a25d284729e231b1228f3c7c4a3
Dante <dante.a.sanchez@gmail.com>
2025-06-27 14:15:54 -0400

    pfm vars

--- scripts/vars_pfm_2.sh
+++ scripts/vars_pfm_2.sh
@@ -1,7 +1,7 @@
 export validator_count=1
 
 # Chain configuration
-export CHAIN_BINARY_NAME="gaiadpfm1"
+export CHAIN_BINARY_NAME="gaiadpfm2"
 export CHAIN_BINARY="./$CHAIN_BINARY_NAME"
 export MNEMONIC_1="abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art"
 export WALLET_1=cosmos1r5v5srda7xfth3hn2s26txvrcrntldjumt8mhl
@@ -27,20 +27,20 @@ export STATE_SYNC_SNAPSHOT_INTERVAL=0
 export STATE_SYNC_SNAPSHOT_KEEP_RECENT=5
 
 export COUNT_WIDTH="01"
-export moniker_prefix='pfm1_'
-export home_prefix='/home/runner/.pfm1_'
-export api_prefix="350"
-export p2p_prefix="360"
-export rpc_prefix="370"
-export grpc_prefix="380"
-export pprof_prefix="390"
-export log_prefix="logpfm1_"
+export moniker_prefix='pfm2_'
+export home_prefix='/home/runner/.pfm2_'
+export api_prefix="450"
+export p2p_prefix="460"
+export rpc_prefix="470"
+export grpc_prefix="480"
+export pprof_prefix="490"
+export log_prefix="logpfm2_"
 
 export whale_home=${home_prefix}$COUNT_WIDTH
 export whale_api=${api_prefix}$COUNT_WIDTH
 export whale_rpc=${rpc_prefix}$COUNT_WIDTH
 export whale_log=${log_prefix}$COUNT_WIDTH
 
-export START_SCRIPT="start-pfm1.sh"
-export STOP_SCRIPT="stop-pfm1.sh"
-export RESET_SCRIPT="reset-pfm1.sh"
+export START_SCRIPT="start-pfm2.sh"
+export STOP_SCRIPT="stop-pfm2.sh"
+export RESET_SCRIPT="reset-pfm2.sh"
--- scripts/vars_pfm_3.sh
+++ scripts/vars_pfm_3.sh
@@ -1,7 +1,7 @@
 export validator_count=1
 
 # Chain configuration
-export CHAIN_BINARY_NAME="gaiadpfm1"
+export CHAIN_BINARY_NAME="gaiadpfm3"
 export CHAIN_BINARY="./$CHAIN_BINARY_NAME"
 export MNEMONIC_1="abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art"
 export WALLET_1=cosmos1r5v5srda7xfth3hn2s26txvrcrntldjumt8mhl
@@ -27,20 +27,20 @@ export STATE_SYNC_SNAPSHOT_INTERVAL=0
 export STATE_SYNC_SNAPSHOT_KEEP_RECENT=5
 
 export COUNT_WIDTH="01"
-export moniker_prefix='pfm1_'
-export home_prefix='/home/runner/.pfm1_'
-export api_prefix="350"
-export p2p_prefix="360"
-export rpc_prefix="370"
-export grpc_prefix="380"
-export pprof_prefix="390"
-export log_prefix="logpfm1_"
+export moniker_prefix='pfm3_'
+export home_prefix='/home/runner/.pfm3_'
+export api_prefix="550"
+export p2p_prefix="560"
+export rpc_prefix="570"
+export grpc_prefix="580"
+export pprof_prefix="590"
+export log_prefix="logpfm3_"
 
 export whale_home=${home_prefix}$COUNT_WIDTH
 export whale_api=${api_prefix}$COUNT_WIDTH
 export whale_rpc=${rpc_prefix}$COUNT_WIDTH
 export whale_log=${log_prefix}$COUNT_WIDTH
 
-export START_SCRIPT="start-pfm1.sh"
-export STOP_SCRIPT="stop-pfm1.sh"
-export RESET_SCRIPT="reset-pfm1.sh"
+export START_SCRIPT="start-pfm3.sh"
+export STOP_SCRIPT="stop-pfm3.sh"
+export RESET_SCRIPT="reset-pfm3.sh"
