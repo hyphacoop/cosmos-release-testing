3f2c91dfff2520d742f580d961f2882d3d18b731
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-14 17:17:21 -0400

    payload size

--- tests/test_feemarket.sh
+++ tests/test_feemarket.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 max_block_utilization=$($CHAIN_BINARY q feemarket params --home $whale_home -o json | jq -r '.max_block_utilization')
-payload_size=$(echo "$max_block_utilization / 10000" | bc)
+payload_size=$(echo "$max_block_utilization / 1000" | bc)
 echo "Max block utilization: $max_block_utilization"
-echo "Payload size: $payload_size"]]
+echo "Payload size: $payload_size"
 # PAYLOAD_SIZE=50000
 
 preload_price=$($CHAIN_BINARY q feemarket gas-prices --home $whale_home -o json | jq -r '.prices[0].amount')
