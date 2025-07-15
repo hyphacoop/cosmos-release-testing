a4e508b12d0098ace0db71da7a94b662e0713c0f
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-15 11:21:48 -0400

    increase payload

--- tests/test_feemarket.sh
+++ tests/test_feemarket.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 max_block_utilization=$($CHAIN_BINARY q feemarket params --home $whale_home -o json | jq -r '.max_block_utilization')
-payload_size=$(echo "$max_block_utilization / 2000" | bc)
+payload_size=$(echo "$max_block_utilization / 1500" | bc)
 echo "Max block utilization: $max_block_utilization"
 echo "Payload size: $payload_size"
 # PAYLOAD_SIZE=50000
