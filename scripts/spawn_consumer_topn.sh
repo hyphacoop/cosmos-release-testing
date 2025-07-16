c113622eb7e9e0b4800803bcca95dc247f7a2c63
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-16 11:19:55 -0400

    parse proposal

--- scripts/spawn_consumer_topn.sh
+++ scripts/spawn_consumer_topn.sh
@@ -15,7 +15,9 @@ spawn_time=$(date -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
 echo "> Spawn time: $spawn_time"
 jq '.' templates/proposal-update-consumer.json
 jq --slurpfile PARAMS init_params.json '.messages[0].initialization_parameters |= $PARAMS[0]' templates/proposal-update-consumer.json > update-$CONSUMER_CHAIN_ID.json
+jq '.' update-$CONSUMER_CHAIN_ID.json
 jq --arg CONSUMERID "$CONSUMER_ID" 'messages[0].consumer_id |= $CONSUMERID' update-$CONSUMER_CHAIN_ID.json > consumer-$CONSUMER_CHAIN_ID.json
+jq '.' consumer-$CONSUMER_CHAIN_ID.json
 jq -r --arg SPAWNTIME "$spawn_time" 'messages[0].initialization_parameters.spawn_time |= $SPAWNTIME' consumer-$CONSUMER_CHAIN_ID.json > spawn-$CONSUMER_CHAIN_ID.json
 
 echo "> Update consumer JSON:"
