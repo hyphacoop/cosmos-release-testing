9ac1f8e2fbaa6a2c76bf7577267061e4d11dcb32
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-16 15:34:20 -0400

    top n condition

--- scripts/setup_consumer.sh
+++ scripts/setup_consumer.sh
@@ -49,12 +49,14 @@ done
 $CHAIN_BINARY q provider list-consumer-chains --home $whale_home -o json --node http://localhost:$whale_rpc | jq -r '.chains[]'
 echo "Consumer ID: $CONSUMER_ID"
 
-echo "> Submitting opt-in txs"
-for i in $(seq 0 $[validator_count-1])
-do
-    echo "> Opting in with ${monikers[i]}."
-    pubkey=$($CHAIN_BINARY comet show-validator --home ${homes[i]})
-    txhash=$($CHAIN_BINARY tx provider opt-in $CONSUMER_ID $pubkey --from ${monikers[i]} --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE --home $whale_home -y -o json | jq -r '.txhash')
+if [ $TOPN -eq "0" ]; then
+    echo "> Submitting opt-in txs"
+    for i in $(seq 0 $[validator_count-1])
+    do
+        echo "> Opting in with ${monikers[i]}."
+        pubkey=$($CHAIN_BINARY comet show-validator --home ${homes[i]})
+        txhash=$($CHAIN_BINARY tx provider opt-in $CONSUMER_ID $pubkey --from ${monikers[i]} --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE --home $whale_home -y -o json | jq -r '.txhash')
+    done
 else
     echo "> Submitting assign-consensus-key txs"
         for i in $(seq 0 $[validator_count-1])
