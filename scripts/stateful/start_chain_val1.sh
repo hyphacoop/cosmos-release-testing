3fd423ace2675b680a4d90819f6364375246e341
ASoT Networks <dev@aseriesoftubez.com>
2025-07-16 13:03:55 -0400

    Stateful feemarket test (#171)

--- scripts/stateful/start_chain_val1.sh
+++ scripts/stateful/start_chain_val1.sh
@@ -84,6 +84,8 @@ toml set --toml-path $HOME_1/config/app.toml state-sync.snapshot-interval --to-i
 
 # Create self-delegation accounts
 echo $MNEMONIC_2 | $CHAIN_BINARY keys add $MONIKER_2 --keyring-backend test --home $HOME_1 --recover
+echo $MNEMONIC_3 | $CHAIN_BINARY keys add $MONIKER_3 --keyring-backend test --home $HOME_1 --recover
+echo $MNEMONIC_4 | $CHAIN_BINARY keys add $MONIKER_4 --keyring-backend test --home $HOME_1 --recover
 
 # rm addrbook
 rm $HOME_1/config/addrbook.json || true
