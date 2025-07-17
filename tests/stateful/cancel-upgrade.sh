12dc59042c880eba3fc4cf790c872f16644771f4
ASoTNetworks <dev@aseriesoftubez.com>
2025-07-17 19:50:26 +0000

    fix query

--- tests/stateful/cancel-upgrade.sh
+++ tests/stateful/cancel-upgrade.sh
@@ -10,4 +10,4 @@ echo "Upgrade height set to: $upgrade_height"
 jq ".messages[].plan .height=$upgrade_height | .messages[].plan .name=\"$UPGRADE_NAME\"" templates/proposal-software-upgrade.json > upgrade_prop.json
 scripts/submit_proposal.sh upgrade_prop.json
 
-$CHAIN_BINARY q upgrade plan -o json
+$CHAIN_BINARY --home $HOME_1 q upgrade plan -o json
