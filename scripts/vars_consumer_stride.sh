b0506c206a3e842f533fc33937a7f47b2351e2d8
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-17 16:51:45 -0400

    spawn v4.0.0

--- scripts/vars_consumer_stride.sh
+++ scripts/vars_consumer_stride.sh
@@ -11,7 +11,7 @@ export CONSUMER_GAS_PRICE=0.005$CONSUMER_DENOM
 
 export ICS_TRANSFORM_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v3.3.0-transform/interchain-security-cd
 export ICS_TRANSFORM_BINARY=cd-transform
-export CONSUMER_ICS="v6.4.0"
+export CONSUMER_ICS="v4.0.0"
 
 export consumer_moniker_prefix='stride_'
 export consumer_home_prefix='/home/runner/.stride_'
