17a35abc0833f7da508f8d1ddd045e1ff2ef742e
ASoTNetworks <dev@aseriesoftubez.com>
2025-07-26 15:46:17 +0000

    fix gaia home

--- tests/stateful/test_vesting.sh
+++ tests/stateful/test_vesting.sh
@@ -9,14 +9,14 @@ echo "[INFO]: feemarket_wallet1: $vesting_wallet1_json"
 vesting_wallet1_addr=$(echo $vesting_wallet1_json | jq -r '.address')
 
 vesting_end_time=$(date -d "+$vesting_time" +%s)
-$CHAIN_BINARY tx vesting create-vesting-account $vesting_wallet1_addr $vesting_amount$DENOM $vesting_end_time --from validator --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM
-$CHAIN_BINARY q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount'
+$CHAIN_BINARY --home $HOME_1 tx vesting create-vesting-account $vesting_wallet1_addr $vesting_amount$DENOM $vesting_end_time --from validator --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM
+$CHAIN_BINARY --home $HOME_1 q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount'
 
 
 echo "[INFO]: Wait until spendable balance matches vesting amount"
 current_block=$(curl -s 127.0.0.1:$VAL1_RPC_PORT/block | jq -r .result.block.header.height)
 count=0
-current_spend_amount=$($CHAIN_BINARY q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount')
+current_spend_amount=$($CHAIN_BINARY --home $HOME_1 q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount')
 
 until [ $current_spend_amount -eq $vesting_amount ]
 do
@@ -24,7 +24,7 @@ do
     if [ "$echo_height" != "$current_block" ]
     then
         echo "[INFO] Current height: $current_block"
-        current_spend_amount=$($CHAIN_BINARY q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount')
+        current_spend_amount=$($CHAIN_BINARY --home $HOME_1 q bank spendable-balances $vesting_wallet1_addr -o json | jq -r '.balances[] | select(.denom="uatom") | .amount')
         echo "[INFO]: Current spendable balance: $current_spend_amount"
         echo_height=$current_block
         count=0
@@ -38,5 +38,5 @@ do
     sleep 1
 done
 
-timestamp=$($CHAIN_BINARY q block --type=height $current_height -o json | jq -r '.header.time')
+timestamp=$($CHAIN_BINARY --home $HOME_1 q block --type=height $current_height -o json | jq -r '.header.time')
 echo $timestamp
