befc214130f1e050110e431e6241611c0b36f228
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-28 09:59:06 -0400

    downgrade changeover to v6.4.0

--- scripts/vars_changeover.sh
+++ scripts/vars_changeover.sh
@@ -1,8 +1,8 @@
 
-export CONSUMER_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v7.0.1/interchain-security-cdd-linux
+export CONSUMER_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v6.4.0/interchain-security-cdd-linux
 export CONSUMER_CHAIN_BINARY_NAME="sovd"
 export CONSUMER_CHAIN_BINARY="./$CONSUMER_CHAIN_BINARY_NAME"
-export CONSUMER_CHAIN_ID="v701-one"
+export CONSUMER_CHAIN_ID="v640-one"
 
 export CONSUMER_DOWNTIME_WINDOW="10"
 export RECIPIENT=consumer1r5v5srda7xfth3hn2s26txvrcrntldju7725yc
@@ -15,11 +15,11 @@ export CONSUMER_ICS="v6.4.0"
 
 export consumer_moniker_prefix='sov_'
 export consumer_home_prefix='/home/runner/.sov_'
-export consumer_api_prefix="171"
-export consumer_p2p_prefix="172"
-export consumer_rpc_prefix="173"
-export consumer_grpc_prefix="174"
-export consumer_pprof_prefix="175"
+export consumer_api_prefix="641"
+export consumer_p2p_prefix="642"
+export consumer_rpc_prefix="643"
+export consumer_grpc_prefix="644"
+export consumer_pprof_prefix="645"
 export consumer_log_prefix="sov_"
 
 export consumer_whale_home=${home_prefix}$COUNT_WIDTH
--- scripts/vars_sovereign.sh
+++ scripts/vars_sovereign.sh
@@ -1,10 +1,10 @@
 
 export validator_count=5
 export CHANGEOVER_UPGRADE_NAME="sovereign-changeover"
-export SOVEREIGN_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v7.0.1/interchain-security-sd-linux
-export CONSUMER_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v7.0.1/interchain-security-cdd-linux
+export SOVEREIGN_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v6.4.0/interchain-security-sd-linux
+export CONSUMER_CHAIN_BINARY_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v6.4.0/interchain-security-cdd-linux
 export CHAIN_BINARY_URL=$SOVEREIGN_CHAIN_BINARY_URL
-export DOWNLOAD_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v7.0.1/interchain-security-cd-linux
+export DOWNLOAD_URL=https://github.com/hyphacoop/cosmos-builds/releases/download/ics-v6.4.0/interchain-security-cd-linux
 # Chain configuration
 export CHAIN_BINARY_NAME="sovd"
 export CHAIN_BINARY="./$CHAIN_BINARY_NAME"
@@ -14,7 +14,7 @@ export WALLET_1=consumer1r5v5srda7xfth3hn2s26txvrcrntldju7725yc
 export MNEMONIC_RELAYER="abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon trouble"
 export WALLET_RELAYER=consumer1jf7j9nvjmnflal5ehaj25p7nsk2t3lkd57l33x
 
-export CHAIN_ID=v701-one
+export CHAIN_ID=v640-one
 export DENOM=ucon
 export VAL_FUNDS="10000000000"
 export VAL_WHALE="100000000"
@@ -33,11 +33,11 @@ export STATE_SYNC_SNAPSHOT_KEEP_RECENT=5
 export COUNT_WIDTH="01"
 export moniker_prefix='sov_'
 export home_prefix='/home/runner/.sov_'
-export api_prefix="171"
-export p2p_prefix="172"
-export rpc_prefix="173"
-export grpc_prefix="174"
-export pprof_prefix="175"
+export api_prefix="641"
+export p2p_prefix="642"
+export rpc_prefix="643"
+export grpc_prefix="644"
+export pprof_prefix="645"
 export log_prefix="sov_"
 
 export whale_home=${home_prefix}$COUNT_WIDTH
