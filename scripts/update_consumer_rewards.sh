c817e567ac04bbd3ebbae8e357d3cfbf3282f39f
Dante <dante.a.sanchez@gmail.com>
2025-07-28 22:21:51 -0400

    home flag

--- scripts/update_consumer_rewards.sh
+++ scripts/update_consumer_rewards.sh
@@ -2,7 +2,7 @@
 PROVIDER_CHANNEL=$1
 
 echo "Patching update template with consumer id..."
-jq -r --arg CONSUMERID "$CONSUMER_ID" '.consumer_id |= $CONSUMERID' templates/update-consumer-infraction.json > update.json
+jq -r --arg CONSUMERID "$CONSUMER_ID" '.consumer_id |= $CONSUMERID' templates/update-consumer.json > update.json
 
 echo "> Delete new_owner_address"
 jq 'del(.new_owner_address)' update.json > update-consumer.json
@@ -17,12 +17,12 @@ jq '.' update.json
 
 echo "Submitting transaction..."
 
-tx="$CHAIN_BINARY tx provider update-consumer update.json --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE$DENOM --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -y -o json"
+tx="$CHAIN_BINARY tx provider update-consumer update.json --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --gas-prices $GAS_PRICE --from $WALLET_1 --keyring-backend test --home $whale_home -y -o json"
 txhash=$($tx | jq -r .txhash)
 # Wait for the proposal to go on chain
 sleep $(($COMMIT_TIMEOUT+2))
 
 echo "Querying txhash..."
-$CHAIN_BINARY q tx $txhash --home $HOME_1 -o json | jq '.'
+$CHAIN_BINARY q tx $txhash --home $whale_home -o json | jq '.'
 
-$CHAIN_BINARY q provider list-consumer-chains -o json --home $HOME_1 | jq '.'
+$CHAIN_BINARY q provider list-consumer-chains -o json --home $whale_home | jq '.'
