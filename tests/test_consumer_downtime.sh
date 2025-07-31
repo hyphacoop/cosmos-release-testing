a05b97eb43c1e0c8149d05365a9bbc3975677823
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-07-31 10:07:51 -0400

    query valset

--- tests/test_consumer_downtime.sh
+++ tests/test_consumer_downtime.sh
@@ -49,6 +49,18 @@ echo "> Waiting for the downtime infraction."
 sleep $(($COMMIT_TIMEOUT*$CONSUMER_DOWNTIME_WINDOW))
 sleep $(($COMMIT_TIMEOUT*10))
 
+echo "> Consumer chain valset:"
+$CONSUMER_CHAIN_BINARY q tendermint-validator-set --home $consumer_whale_home -o json | jq '.'
+
+echo "> Provider chain valset:"
+$CHAIN_BINARY q comet-validator-set --home $whale_home -o json | jq '.'
+
+echo "> Provider chain validators:"
+$CHAIN_BINARY q staking validators --home $whale_home -o json | jq '.'
+
+echo "> Provider chain slashing:"
+$CHAIN_BINARY q slashing signing-infos --home $whale_home -o json | jq '.'
+
 status=$($CHAIN_BINARY q staking validators --home $whale_home -o json | jq -r --arg addr "$valoper" '.validators[] | select(.operator_address==$addr).status')
 echo "> Status: $status"
 if [[ "$status" == "BOND_STATUS_UNBONDING" ]]; then
