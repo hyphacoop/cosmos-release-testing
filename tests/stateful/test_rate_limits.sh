e26bda09a96df29ef7205de0ab781a1006229f3f
ASoTNetworks <dev@aseriesoftubez.com>
2025-08-29 10:50:21 -0400

    test tx below quota

--- tests/stateful/test_rate_limits.sh
+++ tests/stateful/test_rate_limits.sh
@@ -15,8 +15,27 @@ echo "[INFO]: Calculate 1% of supply"
 supply_one_percent=$(printf %.0f $(echo "$total_uatom_supply*0.01" | bc -l))
 echo "[INFO]: 1% is: $supply_one_percent"
 
-echo "[INFO]: transfer 1% of total supply"
-tx_json=$($CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer  $CONSUMERA_CHAN_ID $WALLET_1 $supply_one_percent$DENOM --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
+echo "[INFO]: transfer 1%+2 uatom of total supply"
+set +e
+let tx_1_amount=$supply_one_percent+2
+tx_json=$($CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer  $CONSUMERA_CHAN_ID $WALLET_1 $tx_1_amount$DENOM --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
+if [ $? -eq 0 ]
+then
+    echo "[ERROR]: TX was successful above the quota"
+    exit 1
+else
+    echo "[PASS]: TX was not successful"
+fi
+set -e
+
+echo "[INFO]: transfer 1%-2 uatom of total supply"
+set +e
+let tx_2_amount=$supply_one_percent-2
+tx_json=$($CHAIN_BINARY --home $HOME_1 tx ibc-transfer transfer transfer  $CONSUMERA_CHAN_ID $WALLET_1 $tx_2_amount$DENOM --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
+if [ $? -eq 0 ]
+then
+    echo "[ERROR]: TX was successful"
+fi
 
 # wait for tx block
 tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 5 10
@@ -25,3 +44,4 @@ echo "$tx_json"
 txhash=$(echo $tx_json | jq -r '.txhash')
 
 $CHAIN_BINARY q tx $txhash
+
