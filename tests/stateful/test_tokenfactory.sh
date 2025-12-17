49f5ecc4d44dd0dfb57e21833b01121f07147cb9
ASoTNetworks <dev@aseriesoftubez.com>
2025-12-17 12:10:50 -0500

    submit the metadata proposal

--- tests/stateful/test_tokenfactory.sh
+++ tests/stateful/test_tokenfactory.sh
@@ -287,3 +287,24 @@ else
     echo "$PROPOSAL_STATUS" | jq -r '.'
     exit 1
 fi
+
+# Test setting metadata using proposal
+echo "[INFO]: > Test MsgSetDenomMetadata proposal"
+echo "[DEBUG]: Submitting proposal..."
+source scripts/submit_proposal.sh templates/proposal-tokenfactory-set-denom-metadata.json
+
+denom_metadata=$($CHAIN_BINARY --home $HOME_1 q bank denom-metadata $tf_token1 -o json | jq -r '.')
+
+echo "[INFO]: Current meta for $tf_token1"
+echo "$denom_metadata"
+
+echo "[INFO]: Verify metadata"
+denom_description_query=$(echo $denom_metadata | jq -r '.metadata.description')
+echo "[DEBUG]: metadata from query: $denom_description_query"
+if [ "$denom_description_query" == "$tf_metadata_description" ]
+then
+    echo "[PASS]: Description matches"
+else
+    echo "[ERROR]: Description does not match. Expected: $tf_metadata_description got $denom_description_query"
+    exit 1
+fi
