e06d5b02eace6d771f4631df0ca362b00f237e4f
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-01-05 22:30:32 -0500

    check balances

--- tests/test_factory_pfm_transfer.sh
+++ tests/test_factory_pfm_transfer.sh
@@ -3,6 +3,9 @@ source scripts/vars_pfm_3.sh
 
 factory_denom="factory/$WALLET_1/test"
 
+pre_transfer_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $whale_home -o json | jq -r --arg DENOM "$factory_denom" '.balances[] | select(.denom == $DENOM) | .amount')
+echo "> Pre-transfer balance of $factory_denom on chain A: $pre_transfer_balance"  
+
 echo "Provider chain channel ID: channel-0"
 # provider -> channel-0 chain a -> channel-0 chain b -> channel-0 chain c (A->D)
 target_denom_a_d=ibc/$(echo -n transfer/channel-0/transfer/channel-0/transfer/channel-0/$factory_denom | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
@@ -19,11 +22,14 @@ fi
 echo "Chain D starting balance in expected denom: $d_start_balance"
 
 source scripts/vars.sh
-txhash=$($CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"channel-1\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"channel-1\",\"timeout\":\"10m\"}}}}" 1000000$factory_denom --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home -o json | jq -r '.txhash')
+txhash=$($CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"channel-1\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"channel-1\",\"timeout\":\"10m\"}}}}" 2000000$factory_denom --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home -o json | jq -r '.txhash')
 echo "Waiting for the transfer to reach chain D..."
 date
 sleep $(($COMMIT_TIMEOUT+60))
 date
+
+post_ad_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $whale_home -o json | jq -r --arg DENOM "$factory_denom" '.balances[] | select(.denom == $DENOM) | .amount')
+echo "> Post A-D transfer balance of $factory_denom on chain A: $post_ad_balance"  
 # echo "> Tx hash query:"
 # $CHAIN_BINARY q tx $txhash -o json --home $HOME_1 | jq '.'
 # $CHAIN_BINARY --home $whale_home q bank balances $WALLET_1
@@ -52,12 +58,15 @@ fi
 # fi
 # echo "Chain A starting balance in expected denom: $a_start_balance"
 
-# source scripts/vars_pfm_3.sh
-# $CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"channel-0\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"channel-0\",\"timeout\":\"10m\"}}}}" 1000000$DENOM --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home
-# echo "Waiting for the transfer to reach chain A..."
-# sleep $(($COMMIT_TIMEOUT+60))
+source scripts/vars_pfm_3.sh
+$CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"channel-0\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"channel-0\",\"timeout\":\"10m\"}}}}" 1000000$target_denom_a_d --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home
+echo "Waiting for the transfer to reach chain A..."
+sleep $(($COMMIT_TIMEOUT+60))
 
-# source scripts/vars.sh
+source scripts/vars.sh
+post_da_balance=$($CHAIN_BINARY q bank balances $WALLET_1 --home $whale_home -o json | jq -r --arg DENOM "$factory_denom" '.balances[] | select(.denom == $DENOM) | .amount')
+echo "> Post D-A transfer balance of $factory_denom on chain A: $post_da_balance"
+
 # a_end_balance=$($CHAIN_BINARY --home $whale_home q bank balances $WALLET_1 -o json | jq -r --arg DENOM "$target_denom_d_a" '.balances[] | select(.denom==$DENOM).amount')
 # if [ -z "$a_end_balance" ]; then
 #   a_end_balance=0
