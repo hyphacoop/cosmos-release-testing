916b05f5753948799011f7f313a95121a4b8d251
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-01-08 11:13:11 -0500

    pfm path

--- tests/test_pfm_transfers.sh
+++ tests/test_pfm_transfers.sh
@@ -4,13 +4,24 @@ source scripts/vars_pfm_3.sh
 
 echo "Provider chain channel ID: $pfm_ab_channel_id"
 
-# provider -> pfm 1 -> pfm 2 -> pfm-3 chain c (A->D)
-ad_path=transfer/$pfm_ab_channel_id/transfer/$pfm_bc_channel_id/transfer/$pfm_cd_channel_id/uatom
-echo "A-D path: $ad_path"
+# IBC denom derivation
+# A-D: provider -> pfm 1 -> pfm 2 -> pfm-3 (A->D)
+# 1. Receiving channel in pfm3
+# 2. Receiving channel in pfm2
+# 3. Receiving channel in pfm1
+# 4. Denom in provider
+ad_receive_path=transfer/$pfm_dc_channel_id/transfer/$pfm_cb_channel_id/transfer/$pfm_ba_channel_id/$DENOM
+echo "A-D path: $ad_receive_path"
+target_denom_a_d=ibc/$(echo -n $ad_receive_path | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
 
-target_denom_a_d=ibc/$(echo -n transfer/$pfm_ab_channel_id/transfer/$pfm_bc_channel_id/transfer/$pfm_cd_channel_id/uatom | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
-# pfm 3 -> pfm 2 -> pfm 1 -> provider (D->A)
-target_denom_d_a=ibc/$(echo -n transfer/$pfm_dc_channel_id/transfer/$pfm_cb_channel_id/transfer/$pfm_ba_channel_id/uatom | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+# D-A: provider <- pfm 1 <- pfm 2 <- pfm-3 (D->A)
+# 1. Receiving channel in provider
+# 2. Receiving channel in pfm1
+# 3. Receiving channel in pfm2
+# 4. Denom in pfm3
+da_receive_path=transfer/$pfm_ab_channel_id/transfer/$pfm_bc_channel_id/transfer/$pfm_cd_channel_id/$DENOM
+echo "D-A path: $da_receive_path"
+target_denom_d_a=ibc/$(echo -n $da_receive_path | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
 echo "Target denom A->D: $target_denom_a_d"
 echo "Target denom D->A: $target_denom_d_a"
 
@@ -21,6 +32,11 @@ fi
 echo "Chain D starting balance in expected denom: $d_start_balance"
 
 source scripts/vars.sh
+# PFM path
+# A-D: provider -> pfm 1 -> pfm 2 -> pfm-3 (A->D)
+# 1. Sending channel in provider (transfer channel)
+# 2. Sending channel in pfm1 (first forward)
+# 3. Sending channel in pfm2 (second forward)
 txhash=$($CHAIN_BINARY tx ibc-transfer transfer transfer $pfm_ab_channel_id "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"$pfm_bc_channel_id\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"$pfm_cd_channel_id\",\"timeout\":\"10m\"}}}}" 1000000$DENOM --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home -o json | jq -r '.txhash')
 echo "Waiting for the transfer to reach chain D..."
 date
@@ -56,7 +72,12 @@ fi
 echo "Chain A starting balance in expected denom: $a_start_balance"
 
 source scripts/vars_pfm_3.sh
-$CHAIN_BINARY tx ibc-transfer transfer transfer channel-0 "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"channel-0\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"channel-0\",\"timeout\":\"10m\"}}}}" 1000000$DENOM --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home
+# PFM path
+# D-A: provider <- pfm 1  <- pfm 2 <- pfm-3 (D->A)
+# 1. Sending channel in pfm3 (transfer channel)
+# 2. Sending channel in pfm2 (first forward)
+# 3. Sending channel in pfm1 (second forward)
+$CHAIN_BINARY tx ibc-transfer transfer transfer $pfm_dc_channel_id "pfm" --memo "{\"forward\": {\"receiver\": \"pfm\",\"port\": \"transfer\",\"channel\": \"$pfm_cb_channel_id\",\"timeout\": \"10m\",\"next\": {\"forward\": {\"receiver\": \"$WALLET_1\",\"port\": \"transfer\",\"channel\":\"$pfm_ba_channel_id\",\"timeout\":\"10m\"}}}}" 1000000$DENOM --from $WALLET_1 --gas auto --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home
 echo "Waiting for the transfer to reach chain A..."
 sleep $(($COMMIT_TIMEOUT+60))
 
