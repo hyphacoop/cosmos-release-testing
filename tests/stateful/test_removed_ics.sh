d5d6cef64bb757d024a62ebc101752b377d24561
ASoTNetworks <dev@aseriesoftubez.com>
2026-02-05 13:16:49 -0500

    check for failed tx

--- tests/stateful/test_removed_ics.sh
+++ tests/stateful/test_removed_ics.sh
@@ -3,16 +3,8 @@ set -e
 # Launch a consumer chain
 # $1 sets the backwards compatibility version
 
-transform=$1
-debug=1
 # set spawn time 1 minute from now
 spawn="1 minute"
-
-if [ $debug -eq 1 ]
-then
-    echo "[DEBUG] ADD TEMPLATE templates/create-consumer-stateful.json:"
-    cat templates/create-consumer-stateful.json
-fi
 
 echo "[INFO] Patching add template with spawn time..."
 spawn_time=$(date --date="$spawn" -u --iso-8601=ns | sed s/+00:00/Z/ | sed s/,/./)
@@ -47,16 +39,13 @@ jq -r '.' create-$CONSUMER_CHAIN_ID.json
 cp create-$CONSUMER_CHAIN_ID.json ~/artifact/
 
 echo "[INFO] Submitting transection..."
-transection="$CHAIN_BINARY tx provider create-consumer create-$CONSUMER_CHAIN_ID.json --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -b sync -y -o json"
-echo $transection
-gaiadout=$($transection)
-echo "[INFO] gaiad output:"
-echo "$gaiadout"
-echo "$gaiadout" > ~/artifact/$CONSUMER_CHAIN_ID-tx.txt
+set +e
+$CHAIN_BINARY tx provider create-consumer create-$CONSUMER_CHAIN_ID.json --gas $GAS --gas-adjustment $GAS_ADJUSTMENT --fees $BASE_FEES$DENOM --from $WALLET_1 --keyring-backend test --home $HOME_1 --chain-id $CHAIN_ID -b sync -y -o json
 
-txhash=$(echo "$gaiadout" | jq -r .txhash)
-
-tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 1 10
-
-echo "[INFO]: TX results:"
-$CHAIN_BINARY --output json q tx $txhash --home $HOME_1 | jq -r '.'
+if [ $? -eq 0 ]
+then
+    echo "[PASS]: create-consumer TX failed as expected"
+else
+    echo "[FAIL]: create-consumer TX passed!"
+    exit 1
+fi
