3f7ff6589cdaeee757e2f02bfdae7ef9ce918eb8
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-02-10 22:09:46 -0500

    add rewards to allowlist

--- scripts/update_consumer_rewards.sh
+++ scripts/update_consumer_rewards.sh
@@ -10,7 +10,10 @@ cp update-consumer.json update.json
 
 echo "> Set denom allowlist"
 provider_denom=ibc/$(echo -n transfer/$PROVIDER_CHANNEL/$CONSUMER_DENOM | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
-jq --arg denom "$provider_denom" '.allowlisted_reward_denoms |= { "denoms":[$denom] }' update.json > update-denom.json
+utoken1_denom=ibc/$(echo -n transfer/$PROVIDER_CHANNEL/utoken1 | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+utoken2_denom=ibc/$(echo -n transfer/$PROVIDER_CHANNEL/utoken2 | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+atoken3_denom=ibc/$(echo -n transfer/$PROVIDER_CHANNEL/atoken3 | shasum -a 256 | cut -d ' ' -f1 | tr '[a-z]' '[A-Z]')
+jq --arg denom "$provider_denom" --arg denom2 "$utoken1_denom" --arg denom3 "$utoken2_denom" --arg denom4 "$atoken3_denom" '.allowlisted_reward_denoms |= { "denoms":[$denom,$denom2,$denom3,$denom4] }' update.json > update-denom.json
 cp update-denom.json update.json
 
 jq '.' update.json
