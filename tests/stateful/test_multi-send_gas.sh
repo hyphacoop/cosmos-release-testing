bfa2832780e8d6d677b5f91cfc28f82353c68656
ASoTNetworks <dev@aseriesoftubez.com>
2026-02-13 16:13:07 -0500

    fix tx command

--- tests/stateful/test_multi-send_gas.sh
+++ tests/stateful/test_multi-send_gas.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 generate_wallets_count=$1
 tx_amount=$2
@@ -8,7 +9,7 @@ tx_denom=$3
 python scripts/generate_wallets.py $generate_wallets_count
 wallets=$(jq -r '.[].address' cosmos_wallets.json | xargs)
 
-tx_json=$($CHAIN_BINARY --home $HOME_1 tx bank multi-send --from val $wallets $tx_amount$tx_denom --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
+tx_json=$($CHAIN_BINARY --home $HOME_1 tx bank multi-send $WALLET_1 $wallets $tx_amount$tx_denom --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
 txhash=$(echo $tx_json | jq -r '.txhash')
 
 echo "Query TX output:"
