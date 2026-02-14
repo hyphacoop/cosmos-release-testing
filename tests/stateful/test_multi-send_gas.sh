5c4f5785ad05d5b578b0307b022cfa494006eb83
ASoTNetworks <dev@aseriesoftubez.com>
2026-02-14 17:32:53 -0500

    check error output

--- tests/stateful/test_multi-send_gas.sh
+++ tests/stateful/test_multi-send_gas.sh
@@ -9,24 +9,34 @@ tx_denom=$3
 python scripts/generate_wallets.py $generate_wallets_count
 wallets=$(jq -r '.[].address' cosmos_wallets.json | xargs)
 
+echo "Sending tx..."
 tx_json=$($CHAIN_BINARY --home $HOME_1 tx bank multi-send $WALLET_1 $wallets $tx_amount$tx_denom --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
 txhash=$(echo $tx_json | jq -r '.txhash')
 
 # wait for tx block
 tests/test_block_production.sh 127.0.0.1 $VAL1_RPC_PORT 5 10
 
-tx_json=$($CHAIN_BINARY --home $HOME_1 q tx $txhash -o json)
-code=$(echo $tx_json | jq -r '.code')
+set +e
+tx_query=$($CHAIN_BINARY --home $HOME_1 q tx $txhash -o json)
+if [ $? != 1 ]
+then
+    echo "[ERROR]: TX Failed, TX JSON output:"
+    echo "$tx_json" | jq -r '.'
+    exit 1
+fi
+set -e
+
+code=$(echo $tx_query | jq -r '.code')
 if [ $code -eq 0 ]
 then
     echo "[PASS]: TX was successful"
 fi
 
 # Query gas wanted
-tx_gas_wanted=$(echo $tx_json | jq -r '.gas_wanted')
+tx_gas_wanted=$(echo $tx_query | jq -r '.gas_wanted')
 
 # Query gas used
-tx_gas_used=$(echo $tx_json | jq -r '.gas_used')
+tx_gas_used=$(echo $tx_query | jq -r '.gas_used')
 
 echo "Gas wanted: $tx_gas_wanted"
 echo "Gas used: $tx_gas_used"
