653b2c98b1de66c566d6b7440dcc1abeb46e41fd
ASoTNetworks <dev@aseriesoftubez.com>
2026-02-14 18:19:32 -0500

    add gas-adjustment arg

--- tests/stateful/test_multi-send_gas.sh
+++ tests/stateful/test_multi-send_gas.sh
@@ -4,13 +4,14 @@ set -e
 generate_wallets_count=$1
 tx_amount=$2
 tx_denom=$3
+tx_gas_adjustment=$4
 
 # Generate wallets
 python scripts/generate_wallets.py $generate_wallets_count
 wallets=$(jq -r '.[].address' cosmos_wallets.json | xargs)
 
 echo "Sending tx..."
-tx_json=$($CHAIN_BINARY --home $HOME_1 tx bank multi-send $WALLET_1 $wallets $tx_amount$tx_denom --from val --gas auto --gas-adjustment 5 --gas-prices 3000uatom -y -o json)
+tx_json=$($CHAIN_BINARY --home $HOME_1 tx bank multi-send $WALLET_1 $wallets $tx_amount$tx_denom --from val --gas auto --gas-adjustment $tx_gas_adjustment --gas-prices 3000uatom -y -o json)
 txhash=$(echo $tx_json | jq -r '.txhash')
 
 # wait for tx block
