00dd526e83f8284230e46000270bf91016c19cc6
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-03-03 13:22:23 -0500

    vote with all validators

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -92,6 +92,14 @@ echo "Proposal ID: $proposal_id"
 
 # Vote yes on the proposal
 echo "Submitting the \"yes\" vote to proposal $proposal_id..."
+# Vote with each of the validators starting with val_002
+for i in $(seq - w 01 $validator_count); do
+    val_wallet="$moniker_prefix$i"
+    echo "Voting from $val_wallet"
+    vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from ${!val_wallet} --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home -o json"
+    echo $vote
+    txhash=$($vote | jq -r .txhash)
+done
 vote="$CHAIN_BINARY tx gov vote $proposal_id yes --from $WALLET_1 --keyring-backend test --chain-id $CHAIN_ID --gas $GAS --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJUSTMENT -y --home $whale_home -o json"
 echo $vote
 txhash=$($vote | jq -r .txhash)
