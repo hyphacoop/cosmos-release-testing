10f66927e46c72ce40672b9ef38d7b068e2cff89
Dante <dante.a.sanchez@gmail.com>
2026-03-19 20:55:14 -0400

    reset state

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -37,7 +37,8 @@ $CHAIN_BINARY rollback --home $FORK_HOME
 echo "> Resetting validator state."
 jq '.' $FORK_HOME/data/priv_validator_state.json
 # Set height, round, and step to 0 to ensure a clean state for voting on the upgrade proposal
-jq '.height="0" | .round="0" | .step="0"' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
+jq '.height="0" | .round=0 | .step=0' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
+jq 'del(.signature) | del(.signbytes)' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
 jq '.' $FORK_HOME/data/priv_validator_state.json
 
 # Target: auto-select
