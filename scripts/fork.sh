cdd1191de66dce10a4a0629cc8cbe1065fb26684
Dante <dante.a.sanchez@gmail.com>
2026-03-19 20:43:57 -0400

    reset validator state

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -34,9 +34,11 @@ VALIDATOR=cosmosvaloper1ay4dpm0kjmvtpug28vgw5w32yyjxa5spq248vn
 
 echo "> Rolling back chain to ensure a clean state."
 $CHAIN_BINARY rollback --home $FORK_HOME
-echo "> Deleting vote state."
-ls $FORK_HOME/data
-rm $FORK_HOME/data/priv_validator_state.json
+echo "> Resetting validator state."
+jq '.' $FORK_HOME/data/priv_validator_state.json
+# Set height, round, and step to 0 to ensure a clean state for voting on the upgrade proposal
+jq '.height="0" | .round="0" | .step="0"' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
+jq '.' $FORK_HOME/data/priv_validator_state.json
 
 # Target: auto-select
 # build/gaiad testnet unsafe-start-local-validator \
