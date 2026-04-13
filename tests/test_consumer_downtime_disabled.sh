80e4fd833053e70949be45b56974ffbd525ba99f
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-04-13 16:27:59 -0400

    print  consumer chain log

--- tests/test_consumer_downtime_disabled.sh
+++ tests/test_consumer_downtime_disabled.sh
@@ -32,7 +32,7 @@ do
     logs+=($log)
 done
 
-echo "> Moniker: ${monikers[-3]}"
+echo "> Moniker: ${monikers[1]}"
 wallet=$($CHAIN_BINARY keys list --output json --home $whale_home | jq -r --arg name "${monikers[-3]}" '.[] | select(.name==$name).address')
 echo "> Wallet: $wallet"
 bytes=$($CHAIN_BINARY keys parse $wallet --output json --home $whale_home | jq -r '.bytes')
@@ -43,11 +43,11 @@ $CHAIN_BINARY q staking validator $valoper --home $whale_home -o json | jq '.'
 
 # Jailing
 echo "> Stopping the last validator's consumer node."
-session=${consumer_monikers[-3]}
+session=${consumer_monikers[1]}
 echo "> Session: $session"
 tmux send-keys -t $session C-c
 sleep 2
-tail ${logs[-3]} -n 100
+tail ${logs[1]} -n 100
 echo "> Waiting for the downtime infraction."
 sleep $(($COMMIT_TIMEOUT*$CONSUMER_DOWNTIME_WINDOW))
 sleep $(($COMMIT_TIMEOUT*10))
@@ -65,10 +65,6 @@ echo "> Provider chain validators:"
 $CHAIN_BINARY q staking validators --home $whale_home -o json | jq '.'
 echo "> Provider chain slashing:"
 $CHAIN_BINARY q slashing signing-infos --home $whale_home -o json | jq '.'
-
-echo "> Print consumer chain log"
-tail ${logs[-3]} -n 1000
-
 
 $CHAIN_BINARY q staking validator $valoper --home $whale_home -o json | jq '.'
 # $CHAIN_BINARY q staking validators --home $whale_home -o json | jq -r --arg addr "$valoper" '.validators[] | select(.operator_addres==$addr)'
