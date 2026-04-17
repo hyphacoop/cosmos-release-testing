1d38f3f4432bfadde72bac40b3fbf2ccbe9663b0
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-04-17 08:41:00 -0400

    state sync before validator creation

--- scripts/fork.sh
+++ scripts/fork.sh
@@ -40,10 +40,7 @@ echo "> Add new validator to keyring"
 FORK_MNEMONIC="abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon cabbage abandon garage"
 $FORK_BINARY keys add fork-val --keyring-backend test --home $FORK_HOME --recover --output json <<< "$FORK_MNEMONIC" > /dev/null
 
-# echo "> Rolling back chain to ensure a clean state."
-# $CHAIN_BINARY rollback --hard --home $FORK_HOME
 echo "> Resetting validator state."
-# jq '.' $FORK_HOME/data/priv_validator_state.json
 # Set height, round, and step to 0 to ensure a clean state for voting on the upgrade proposal
 jq '.height="0" | .round=0 | .step=0' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
 jq 'del(.signature) | del(.signbytes)' $FORK_HOME/data/priv_validator_state.json > $FORK_HOME/data/priv_validator_state.json.tmp && mv $FORK_HOME/data/priv_validator_state.json.tmp $FORK_HOME/data/priv_validator_state.json
